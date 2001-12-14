309fa04decb2e631ca56bfe57aedc8848a7b4431
Chris Lattner <sabre@nondot.org>
2001-12-14 16:13:40 +0000

    Initial checkin of a bunch of testcases
    llvm-svn: 1452

---
+++ SingleSource/Regression/C/test_indvars.c
@@ -0,0 +1,23 @@
+
+
+
+void test_indvars(int *Array1, int Array2[100][200]) {
+  unsigned i, j;
+  Array1[1] = Array2[3][6] = 12345;
+
+  for (i = 0; i < 100; i+=2)
+    Array1[i] = i;           // Step by non unit amount
+
+  for (i = 3; i < 103; i++)
+    Array1[i] = i+4;         // Step with an offset
+
+  for (i = 13; i < 100; i++)
+    for (j = 0; j < 100; j+=3)       // 2d array access
+      Array2[i][j/3] = Array2[i][i];
+} 
+
+
+int main() {
+  int Array[100][200];
+  test_indvars(Array[0], Array);
+}
---
+++ SingleSource/sumarray.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+int SumArray(int Array[], int Num) {
+  unsigned i, Result = 0;
+  Array[34] = 1234;
+
+  for (i = 0; i < Num; ++i)
+    Result += Array[i];
+
+  return Result;
+}
+
+int main() {
+  int *Array = (int*)malloc(sizeof(int)*100);
+  int i;
+
+  for (i = 0; i < 100; i += 2)
+  	Array[i] = i*4;
+  for (i = 1; i < 100; i += 2)
+  	Array[i] = i*2;
+
+  return SumArray(Array, 100);
+}
