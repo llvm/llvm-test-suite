196921d8434c4c7b2b6dcb79759e0927365037c0
Chris Lattner <sabre@nondot.org>
2002-01-23 05:53:59 +0000

    Initial checkin
    llvm-svn: 1560

---
+++ DiffOutput.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Program:  DiffOutput.sh
+#
+# Synopsis: Check two output files for program executions and make sure they
+#           match.
+#
+# Syntax:  ./DiffOutput [lli|llc] <testname>
+#
+
+# DIFFOUTPUT - The output filename to make
+DIFFOUTPUT=Output/$2.diff.$1
+
+# Diff the two files.
+diff -c Output/$2.out.nat Output/$2.out.$1 > $DIFFOUTPUT || (
+  # They are different!
+  echo "******************** TEST '$2' FAILED! ********************"
+  echo "Execution Context Diff:"
+  cat $DIFFOUTPUT
+  rm $DIFFOUTPUT
+  echo "******************** TEST '$2' FAILED! ********************"
+)
