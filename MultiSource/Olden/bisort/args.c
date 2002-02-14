e53b5b9d7392a48c03464580e4106121fc2276a0
Chris Lattner <sabre@nondot.org>
2002-02-14 16:22:41 +0000

    Initial checkin of bitonic benchmark, cleaned up to be 64bit clean.
    llvm-svn: 1757

---
+++ MultiSource/Olden/bisort/args.c
@@ -0,0 +1,36 @@
+/* For copyright information, see olden_v1.0/COPYRIGHT */
+#include <stdlib.h>
+
+extern int NumNodes,NDim;
+extern int flag;
+
+int mylog(int num) {
+  int j=0,k=1;
+  
+  while(k<num) { k*=2; j++; }
+  return j;
+} 
+
+int dealwithargs(int argc, char *argv[])
+{
+  int size;
+
+  if (argc > 3)
+    flag = atoi(argv[3]);
+  else
+    flag = 0;
+
+  if (argc > 2) 
+    NumNodes = atoi(argv[2]);
+  else 
+    NumNodes = 4;
+
+  if (argc > 1)
+    size = atoi(argv[1]);
+  else
+    size = 16;
+
+  NDim = mylog(NumNodes);
+  return size;
+}
+
