58b85e52b9d53ac1dcb8321e04a65d722579919c
Chris Lattner <sabre@nondot.org>
2002-03-15 16:13:44 +0000

    Remove #ifdef'd out code
    llvm-svn: 1878

--- MultiSource/Benchmarks/Olden/treeadd/par-alloc.c
+++ MultiSource/Benchmarks/Olden/treeadd/par-alloc.c
@@ -11,28 +11,10 @@
 #include "tree.h"
 extern void *malloc(unsigned);
 
-tree_t *TreeAlloc (int level, int lo, int proc)
-{
-
+tree_t *TreeAlloc (int level, int lo, int proc) {
   if (level == 0)
     return NULL;
   else {
-#ifdef FUTURES    
-    struct tree *new, *right;
-    int mid, lo_tmp;
-    future_cell_int fleft;
-    
-    new = (struct tree *) ALLOC(lo, sizeof(tree_t));
-    NOTEST();
-    FUTURE(level -1, lo+proc/2, proc/2,TreeAlloc,&fleft);
-    right=TreeAlloc(level-1,lo,proc/2);
-    new->val = 1;
-    TOUCH(&fleft);
-    new->left = (struct tree *) fleft.value;
-    new->right = (struct tree *) right;
-    RETEST();
-    return new;
-#else
     struct tree *new, *right, *left;
     new = (struct tree *) malloc(sizeof(tree_t));
     left = TreeAlloc(level -1, lo+proc/2, proc/2);
@@ -41,7 +23,5 @@ tree_t *TreeAlloc (int level, int lo, int proc)
     new->left = (struct tree *) left;
     new->right = (struct tree *) right;
     return new;
-#endif
   }
-
 }
