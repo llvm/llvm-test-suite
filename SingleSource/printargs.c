c0626358e6f07be3812d6c28abb358c3dd19f78f
Vikram S. Adve <vadve@cs.uiuc.edu>
2002-03-18 02:59:00 +0000

    Print something.
    llvm-svn: 1886

--- SingleSource/printargs.c
+++ SingleSource/printargs.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
 
 int puts(const char *);
 
 void main(int argc, char **argv) {
 	int i;
+	printf("#Args = %d. They are:\n", argc-1);
 	for (i = 1; i < argc; ++i)
 		puts(argv[i]);
 }
