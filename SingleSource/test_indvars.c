deea9fe95a5cf5c6b9721c3d86df731c6ab15336
Vikram S. Adve <vadve@cs.uiuc.edu>
2002-03-18 03:00:50 +0000

    Stick to C syntax.
    llvm-svn: 1889

--- SingleSource/test_indvars.c
+++ SingleSource/test_indvars.c
@@ -6,21 +6,21 @@ static void test_indvars(int *Array1, int Array2[100][200]) {
   Array1[1] = Array2[3][6] = 12345;
 
   for (i = 0; i < 100; i+=2)
-    Array1[i] = i;           // Step by non unit amount
+    Array1[i] = i;           /* Step by non unit amount */
 
   for (i = 3; i < 103; i++)
-    Array1[i] = i+4;         // Step with an offset
+    Array1[i] = i+4;         /* Step with an offset */
 
   for (i = 13; i < 100; i++)
-    for (j = 0; j < 100; j+=3)       // 2d array access
+    for (j = 0; j < 100; j+=3)       /* 2d array access */
       Array2[i][j/3] = Array2[i][i];
 } 
 
 
 int main() {
   int Array[100][200], i, j;
-  double sum = 0;
-
+  double sum = 0.0;
+  
   for (i=0; i < 100; i+=2)
     for (j=0; j < 200; j++)
       Array[i][j] = 0;
@@ -29,7 +29,8 @@ int main() {
   for (i=0; i < 100; i+=2)
     for (j=0; j < 200; j++)
       sum += Array[i][j];
+  
   printf("Checksum = %.0lf\n", sum);
-
+  
   return 0;
 }
