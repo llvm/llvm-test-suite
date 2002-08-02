6a703d42857cd05d0a32611552b6542e01b7d63c
Chris Lattner <sabre@nondot.org>
2002-08-02 22:19:28 +0000

    Make the testcase be more standards compliant.
    llvm-svn: 3229

--- SingleSource/UnitTests/2002-05-02-ManyArguments.c
+++ SingleSource/UnitTests/2002-05-02-ManyArguments.c
@@ -6,7 +6,7 @@ void printfn(int a, short b, double C, float D, signed char E, char F, void *G, 
 	printf("%d, 0x%p, 0x%p, %d, %ld\n", F, G, H, I, J);
 }
 
-int main(int argc) {
+int main() {
 	printfn(12, 2, 123.234, 1231.12312f, -12, 23, (void*)123456, 0, 1234567, 123124124124LL);
 	return 0;
 }
