af741a0e820d82ba038d4b72dddd576fda85cda4
Vikram S. Adve <vadve@cs.uiuc.edu>
2002-08-06 18:31:09 +0000

    Print hex values along with integers.  Also do more arithmetic.
    llvm-svn: 3252

--- SingleSource/UnitTests/2002-05-02-CastTest.c
+++ SingleSource/UnitTests/2002-05-02-CastTest.c
@@ -6,32 +6,41 @@ int
 main(int argc, char** argv)
 {
   char  c1 = (argc >= 2)? atoi(argv[1]) : 100;  /* 100 = 'd' */
-  short s1 = (argc >= 3)? atoi(argv[2]) : -769; /* 0xf7ff = -769 */
+  short s1 = (argc >= 3)? atoi(argv[2]) : -769; /* 0xfcff = -769 */
   
   unsigned char ubc0 = (unsigned char) c1;      /* 100 = 'd' */
   uint8_t  ubs0 = (uint8_t) s1;                 /* 0xff = 255 */
-  int8_t   bs0  = (int8_t) s1;                  /* 0xff = -1 */
-  
+  uint8_t  ubs1 = ubs0+1;                       /* ((uint8_t) 0xff) + 1) = 0 */
+
+  int8_t   bs0  = (int8_t) ubs0;                /* (int8_t) 0xff = -1 */
+  int8_t   bs1  = (int8_t) (ubs0 + 1U);         /* (int8_t) (0xff + 0x1) = 0 */
+
   unsigned char  uc2 = (unsigned char) c1;      /* 100 = 'd' */
-  unsigned short us2 = (unsigned short) s1;     /* 0xf7ff = 64767 */
-  
+  unsigned short us2 = (unsigned short) s1;     /* 0xfcff = 64767 */
+
   int ic3 = (int) c1;                           /* 100 = 'd' */
-  int is3 = (int) s1;                           /* 0xfffff7ff = -769 */
-  
+  int is3 = (int) s1;                           /* 0xfffffcff = -769 */
+  int is4 = ((int) ubs0) + 1;                   /* ((int) 0xff) + 1 = 256 */
+  int is5 = ((int)  bs0) + 1;                   /* ((int) -1)   + 1 = 0 */
+
   unsigned int uic4 = (unsigned int) c1;        /*  100 = 'd' */
   unsigned int uis4 = (unsigned int) s1;        /* 0xfffff7ff = 4294966527 */
   
-  printf("ubc0 = '%c'\n", ubc0);
-  printf("ubs0 = %u\n",   ubs0);
-  printf("bs0  = %d\n",   bs0);
-  printf("c1   = '%c'\n", c1);
-  printf("s1   = %d\n",   s1);
-  printf("uc2  = '%c'\n", uc2);
-  printf("us2  = %u\n",   us2);
-  printf("ic3  = '%c'\n", ic3);
-  printf("is3  = %d\n",   is3);
-  printf("uic4 = '%c'\n", uic4);
-  printf("uis4 = %u\n",   uis4);
+  printf("ubc0 = '%c'\t   [0x%p]\n", ubc0, ubc0);
+  printf("ubs0 = %u\t   [0x%p]\n",   ubs0, ubs0);
+  printf("ubs1 = %u\t   [0x%p]\n",   ubs1, ubs1);
+  printf("bs0  = %d\t   [0x%p]\n",   bs0,  bs0);
+  printf("bs1  = %d\t   [0x%p]\n",   bs1,  bs1);
+  printf("c1   = '%c'\t   [0x%p]\n", c1,   c1);
+  printf("s1   = %d\t   [0x%p]\n",   s1,   s1);
+  printf("uc2  = '%c'\t   [0x%p]\n", uc2,  uc2);
+  printf("us2  = %u\t   [0x%p]\n",   us2,  us2);
+  printf("ic3  = '%c'\t   [0x%p]\n", ic3,  ic3);
+  printf("is3  = %d\t   [0x%p]\n",   is3,  is3);
+  printf("is4  = %d\t   [0x%p]\n",   is4,  is4);
+  printf("is5  = %d\t   [0x%p]\n",   is5,  is5);
+  printf("uic4 = '%c'\t   [0x%p]\n", uic4, uic4);
+  printf("uis4 = %u  [0x%p]\n",   uis4, uis4);
   
   return 0;
 }
