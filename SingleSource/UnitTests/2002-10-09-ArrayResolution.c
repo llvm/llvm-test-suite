d2715524718a3a31674e39c0ec81cea2c81048bc
Chris Lattner <sabre@nondot.org>
2002-10-09 16:48:12 +0000

    New testcase distilled from SPECINT gzip benchmark
    llvm-svn: 4094

---
+++ SingleSource/UnitTests/2002-10-09-ArrayResolution.c
@@ -0,0 +1,13 @@
+int Array[];
+int Array[50000];
+int Foo;            /* If writing to the wrong "Array", this will get clobbered */
+
+int main() { 
+  unsigned i;
+  for (i = 0; i != 50000; ++i)
+    Array[i] = 5;
+
+  printf("%d\n", Foo);
+  return 0;
+}
+
