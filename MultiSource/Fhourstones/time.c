ac6d5bc2bee9f67aa54f2e040a736dee85442bdf
Chris Lattner <sabre@nondot.org>
2003-05-02 18:40:36 +0000

    Don't break diffs by actually including timeing information
    llvm-svn: 5981

--- MultiSource/Fhourstones/time.c
+++ MultiSource/Fhourstones/time.c
@@ -9,6 +9,8 @@
 int64 millisecs()
 {
   struct rusage rusage;
+  static int64 Time = 0;
+  return ++Time; /* DO NOT TIME FOR LLVM */
 
   getrusage(RUSAGE_SELF,&rusage);
   return rusage.ru_utime.tv_sec * 1000 + rusage.ru_utime.tv_usec / 1000;
