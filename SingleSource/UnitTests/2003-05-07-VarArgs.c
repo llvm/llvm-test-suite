e86375e2dbc63825763cdb21602c5cae5193d2f1
Chris Lattner <sabre@nondot.org>
2003-05-08 19:43:55 +0000

    Beef up the test a bit
    llvm-svn: 6045

--- SingleSource/UnitTests/2003-05-07-VarArgs.c
+++ SingleSource/UnitTests/2003-05-07-VarArgs.c
@@ -17,9 +17,15 @@ void test(char *fmt, ...) {
       s = va_arg(ap, char *);
       printf("string %s\n", s);
       break;
-    case 'd':           /* int */
+    case 'i':           /* int */
       d = va_arg(ap, int);
       printf("int %d\n", d);
+      break;
+    case 'd':
+      printf("double %f\n", va_arg(ap, double));
+      break;
+    case 'l':
+      printf("long long %lld\n", va_arg(ap, long long));
       break;
     case 'c':           /* char */
       /* need a cast here since va_arg only
@@ -32,6 +38,9 @@ void test(char *fmt, ...) {
 }
 
 int main() {
-  test("ssdcd", "abc", "def", -123, 'a', 123);
+  test("ssici", "abc", "def", -123, 'a', 123);
+
+  /* test promotion & 64-bit types */
+  test("ddil", 1.0, 2.0f, (short)32764, 12345677823423LL);
   return 0;
 }
