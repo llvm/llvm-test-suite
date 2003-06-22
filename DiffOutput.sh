d51141cb34c6d62b0b5482e5f1993019af39605b
Chris Lattner <sabre@nondot.org>
2003-06-22 20:21:04 +0000

    Well the --speed-large-files option did not fix the crafty test on the nightly tester, so remove it.
    llvm-svn: 6850

--- DiffOutput.sh
+++ DiffOutput.sh
@@ -20,7 +20,7 @@ else
 fi
 
 # Diff the two files.
-$DIFF -C 2 --speed-large-files Output/$2.out-nat Output/$2.out-$1 > $DIFFOUTPUT || (
+$DIFF -u Output/$2.out-nat Output/$2.out-$1 > $DIFFOUTPUT || (
   # They are different!
   echo "******************** TEST '$2' FAILED! ********************"
   echo "Execution Context Diff:"
