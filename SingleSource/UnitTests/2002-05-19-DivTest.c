b3cdd318f747df0a65b8d6fdb3118e8c721ae26d
Vikram S. Adve <vadve@cs.uiuc.edu>
2003-07-07 22:46:59 +0000

    Made this much more useful by using (a) larger shifts than just 1, (b) shifts with different ranges, and (c) separate tests for 32-bit and 64-bit operands.
    llvm-svn: 7124

--- SingleSource/UnitTests/2002-05-19-DivTest.c
+++ SingleSource/UnitTests/2002-05-19-DivTest.c
@@ -1,11 +1,26 @@
 extern int printf(const char *, ...);
 
+void testL(long Arg) {
+  printf("%ld\n", Arg / (1LL << 4));
+  printf("%ld\n", Arg / (1LL << 46));
+}
+
 void test(int Arg) {
-   printf("%d\n", Arg / 2);
+  printf("%d\n", Arg / (1 << 0));
+  printf("%d\n", Arg / (1 << 4));
+  printf("%d\n", Arg / (1 << 18));
+  printf("%d\n", Arg / (1 << 30));
 }
 
 int main() {
-	test(-21);
-	test(-22);
-	return 0;
+  int B20 = - (1 << 20);
+  long B53 = - (1LL << 53);
+
+  test(B20 + 32);
+  test(B20 + 33);
+
+  testL(B53 + 64);
+  testL(B53 + 65);
+
+  return 0;
 }
