2c8838effa186ccdb89ad98f3497b1ddaeb08413
John Criswell <criswell@uiuc.edu>
2004-09-13 21:54:12 +0000

    Add comment about (more or less) copying this from the Linux manual page. And yes, I'm paranoid.
    llvm-svn: 16316

--- MultiSource/Applications/kimwitu++/getopt.h
+++ MultiSource/Applications/kimwitu++/getopt.h
@@ -1,3 +1,7 @@
+/*
+ * These values are modified forms of what you will find in the Linux getopt(3)
+ * manual page.  Why I can't get these from unistd.h is beyond me.
+ */
 #if 1
 extern int getopt (...);
 extern char * optarg;
