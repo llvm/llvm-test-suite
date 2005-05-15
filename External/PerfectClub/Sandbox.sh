8993635e3f85a3c6a0dbfa08fbdd8e3221d56b02
CVS to SVN Conversion <nobody@llvm.org>
2005-05-15 21:18:45 +0000

    This commit was manufactured by cvs2svn to create branch 'release_15'.
    llvm-svn: 22067

---
+++ External/PerfectClub/Sandbox.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# Program:  Sandbox.sh
+#
+# Synopsis: This script is used to provide a sandbox for Perfect Club programs
+#           to execute in.  It provides an isolated environment, the input files
+#           necessary, and then finally runs the program.
+#
+# Syntax:  ./Sandbox.sh <uid> <output_file> <ref input> <program> <arguments>
+#
+
+TEST_UID=$1
+OUTPUT_FILE=$2
+INPUT_DIR=$3
+shift;shift;shift
+
+mkdir Output/$TEST_UID 2> /dev/null
+cd Output/$TEST_UID
+rm -rf *
+
+# Copy all of the test input files into the current directory...
+cp -r $INPUT_DIR/* .
+
+# Run the program now...
+echo Running: $*
+$*
+
+# This script is always successful...
+exit 0
+
