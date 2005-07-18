17d5335ad630ca82ca52792dcb6446b397ba905f
Chris Lattner <sabre@nondot.org>
2005-07-18 04:29:50 +0000

    thorough tests for int-to-fp casting of all sizes
    llvm-svn: 22463

---
+++ SingleSource/UnitTests/2005-07-17-INT-To-FP.c
@@ -0,0 +1,19 @@
+// Test the various SINT-TO-FP and UINT-TO-FP conversions.
+#include <stdio.h>
+
+int main() {
+  unsigned i;
+  for (i = 0; i < 64; ++i) {
+    printf("%d %f, %f, %f, %f\n", i,
+           (double)(signed char)(i << 2),            // i8
+           (double)(signed short)(i << 10),          // i16
+           (double)(signed int)(i << 26),            // i32
+           (double)(signed long long)((long long)i << 58ULL));  // i64
+
+    printf("%d %f, %f, %f, %f\n", i,
+           (double)(unsigned char)(i << 2),            // i8
+           (double)(unsigned short)(i << 10),          // i16
+           (double)(unsigned int)(i << 26),            // i32
+           (double)(unsigned long long)((unsigned long long)i << 58ULL));  // i64
+  }
+}
