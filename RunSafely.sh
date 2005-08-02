4193902d9f120e3ec1cda6803690fc45bf77580b
Chris Lattner <sabre@nondot.org>
2005-08-02 22:04:00 +0000

    Prevent llvm-test from filling up /cores with giant core files on darwin.
    llvm-svn: 22599

--- RunSafely.sh
+++ RunSafely.sh
@@ -23,6 +23,15 @@ SYSTEM=`uname -s`
 case $SYSTEM in
   CYGWIN*) 
     ;;
+  Darwin*)
+    # Disable core file emission, the script doesn't find it anyway because it is put 
+    # into /cores.
+    ulimit -c 0
+    ulimit -t $ULIMIT
+    # To prevent infinite loops which fill up the disk, specify a limit on size of
+    # files being output by the tests. 10 MB should be enough for anybody. ;)
+    ulimit -f 10485760
+    ;;
   *)
     ulimit -t $ULIMIT
     ulimit -c unlimited
