ff9267bffda36934237a7e410092431ae1947fef
Jim Laskey <jlaskey@mac.com>
2005-08-18 17:16:22 +0000

    Adding edge case tests for int to fp casting.
    llvm-svn: 22860

--- SingleSource/UnitTests/2005-07-17-INT-To-FP.c
+++ SingleSource/UnitTests/2005-07-17-INT-To-FP.c
@@ -1,8 +1,24 @@
 // Test the various SINT-TO-FP and UINT-TO-FP conversions.
 #include <stdio.h>
 
+int tests[] = {
+  0x80000000,
+  -123456789,
+  -10,
+  -2,
+  -1,
+  0,
+  1,
+  2,
+  10,
+  123456789,
+  0x7FFFFFFF
+};
+
 int main() {
+
   unsigned i;
+  // byte boundary tests
   for (i = 0; i < 64; ++i) {
     printf("%d %f, %f, %f, %f\n", i,
            (double)(signed char)(i << 2),            // i8
@@ -15,5 +31,12 @@ int main() {
            (double)(unsigned short)(i << 10),          // i16
            (double)(unsigned int)(i << 26),            // i32
            (double)(unsigned long long)((unsigned long long)i << 58ULL));  // i64
+  }
+  // edge case tests
+  for (unsigned i = 0; i < (sizeof(tests) / sizeof(int)); i++) {
+    printf("%d %f %f %f %f\n", (double)(unsigned)tests[i],
+                               (double)(  signed)tests[i],
+                               (float) (unsigned)tests[i],
+                               (float) (  signed)tests[i]);
   }
 }
