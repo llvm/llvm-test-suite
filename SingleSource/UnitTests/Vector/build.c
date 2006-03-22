c0b6edf50022f46041f53d53e4a347bc8a5ab98d
Chris Lattner <sabre@nondot.org>
2006-03-22 01:48:35 +0000

    add some trivial testcases.
    llvm-svn: 26937

---
+++ SingleSource/UnitTests/Vector/build.c
@@ -0,0 +1,14 @@
+#include "helpers.h"
+
+int main(int argc, char **Argv) {
+	float X = 1.234;
+	if (argc == 1123) X = 2.38213;
+	FV A;
+        A.V = (v4sf){ X, X, X, X };  // splat
+        A.V = A.V * A.V;
+        printFV(&A);
+        A.V = (v4sf){ X, X, 0, 0 };
+	A.V = A.V+A.V;
+        printFV(&A);
+}
+
---
+++ SingleSource/UnitTests/Vector/helpers.h
@@ -0,0 +1,10 @@
+typedef float v4sf __attribute__ ((__vector_size__ (16)));
+
+typedef union {
+  v4sf V;
+  float A[4];
+} FV;
+
+static void printFV(FV *F) {
+	printf("%f %f %f %f\n", F->A[0], F->A[1], F->A[2], F->A[3]);
+}
