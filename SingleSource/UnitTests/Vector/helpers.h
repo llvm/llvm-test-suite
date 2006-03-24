f1e9deed585c53ae520a2fa9a5beb759b886c582
Evan Cheng <evan.cheng@apple.com>
2006-03-24 01:18:59 +0000

    More tests.
    llvm-svn: 27025

--- SingleSource/UnitTests/Vector/helpers.h
+++ SingleSource/UnitTests/Vector/helpers.h
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
-typedef float v4sf __attribute__ ((__vector_size__ (16)));
+typedef float  v4sf __attribute__ ((__vector_size__ (16)));
+typedef double v2sd __attribute__ ((__vector_size__ (16)));
 
 typedef union {
   v4sf V;
   float A[4];
 } FV;
 
+typedef union {
+  v2sd V;
+  double A[2];
+} DV;
+
 static void printFV(FV *F) {
 	printf("%f %f %f %f\n", F->A[0], F->A[1], F->A[2], F->A[3]);
+}
+
+static void printDV(DV *D) {
+	printf("%g %g\n", D->A[0], D->A[1]);
 }
