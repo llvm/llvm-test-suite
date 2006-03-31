74759beaba1c3a26b096db05cab2bd724f598acb
Chris Lattner <sabre@nondot.org>
2006-03-31 02:11:46 +0000

    new testcase, which requires expansion of cross-block values.
    llvm-svn: 27279

--- SingleSource/UnitTests/Vector/helpers.h
+++ SingleSource/UnitTests/Vector/helpers.h
@@ -3,6 +3,10 @@
 typedef float  v4sf __attribute__ ((__vector_size__ (16)));
 typedef double v2sd __attribute__ ((__vector_size__ (16)));
 
+// These require legalization.
+typedef float  v8sf __attribute__ ((__vector_size__ (32)));
+typedef double v8sd __attribute__ ((__vector_size__ (64)));
+
 typedef union {
   v4sf V;
   float A[4];
@@ -13,10 +17,20 @@ typedef union {
   double A[2];
 } DV;
 
+typedef union {
+  v8sd V;
+  double A[8];
+} D8V;
+
 static void printFV(FV *F) {
 	printf("%f %f %f %f\n", F->A[0], F->A[1], F->A[2], F->A[3]);
 }
 
 static void printDV(DV *D) {
 	printf("%g %g\n", D->A[0], D->A[1]);
+}
+
+static void printD8V(D8V *D) {
+	printf("%g %g %g %g %g %g %g %g\n", D->A[0], D->A[1], D->A[2], D->A[3],
+                                            D->A[4], D->A[5], D->A[6], D->A[7]);
 }
