4a49f71fe2fe4896e5274aa4e0584ab7600596a9
Chris Lattner <sabre@nondot.org>
2006-09-16 07:06:43 +0000

    Actually copy over the input for 444.namd
    llvm-svn: 30408

--- External/SPEC/Sandbox.sh
+++ External/SPEC/Sandbox.sh
@@ -21,8 +21,12 @@ rm -rf *
 # Copy all of the test input files into the current directory.
 cp -r $INPUT_DIR/* .
 
-# If there is an "all" input directory, take it as well.
-cp -r $INPUT_DIR/../../all/input/* . > /dev/null 2>&1 
+# If there is an "all" input directory, take it as well.  This
+# effectively copies over $INPUT_DIR/../../all/input, but works if the
+# actual $INPUT_DIR directory doesn't exist, as is the case with 444.namd.
+UP1=`dirname $INPUT_DIR`
+UP2=`dirname $UP1`
+cp -r $UP2/all/input/* . > /dev/null 2>&1 
 
 # If any of the input files is a .bz2 file, uncompress it now.
 bunzip2 *.bz2 > /dev/null 2>&1
