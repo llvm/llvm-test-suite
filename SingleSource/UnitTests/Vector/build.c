e3cdca400506c81eaa3aece366564eb53bb3219c
Chris Lattner <sabre@nondot.org>
2006-12-01 19:25:13 +0000

    correct this test
    llvm-svn: 32082

--- SingleSource/UnitTests/Vector/build.c
+++ SingleSource/UnitTests/Vector/build.c
@@ -5,8 +5,11 @@ int main(int argc, char **Argv) {
 	if (argc == 1123) X = 2.38213;
 	FV A;
         A.V = (v4sf){ X, X, X, X };  // splat
-        //A.V = A.V * A.V;
-        printf("%f", A.A[1]);
+        A.V = A.V * A.V;
+        printFV(&A);
+        A.V = (v4sf){ X, X, 0, 0 };
+	A.V = A.V+A.V;
+        printFV(&A);
 	return 0;
 }
 
