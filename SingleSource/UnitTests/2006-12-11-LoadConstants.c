d18cde64b9a3cebd8dc207617ee09924da7864a0
Jim Laskey <jlaskey@mac.com>
2006-12-12 13:15:29 +0000

    Add more constants.
    llvm-svn: 32480

--- SingleSource/UnitTests/2006-12-11-LoadConstants.c
+++ SingleSource/UnitTests/2006-12-11-LoadConstants.c
@@ -92,6 +92,72 @@ long long uuuz() { return 0x7fff7fff7fff0000LL; }
 long long uuus() { return 0x7fff7fff7fffffffLL; }
 long long uuuu() { return 0x7fff7fff7fff7fffLL; }
 
+long long bit00() { return 0x0000000000000001LL; }
+long long bit01() { return 0x0000000000000002LL; }
+long long bit02() { return 0x0000000000000004LL; }
+long long bit03() { return 0x0000000000000008LL; }
+long long bit04() { return 0x0000000000000010LL; }
+long long bit05() { return 0x0000000000000020LL; }
+long long bit06() { return 0x0000000000000040LL; }
+long long bit07() { return 0x0000000000000080LL; }
+long long bit08() { return 0x0000000000000100LL; }
+long long bit09() { return 0x0000000000000200LL; }
+long long bit10() { return 0x0000000000000400LL; }
+long long bit11() { return 0x0000000000000800LL; }
+long long bit12() { return 0x0000000000001000LL; }
+long long bit13() { return 0x0000000000002000LL; }
+long long bit14() { return 0x0000000000004000LL; }
+long long bit15() { return 0x0000000000008000LL; }
+long long bit16() { return 0x0000000000010000LL; }
+long long bit17() { return 0x0000000000020000LL; }
+long long bit18() { return 0x0000000000040000LL; }
+long long bit19() { return 0x0000000000080000LL; }
+long long bit20() { return 0x0000000000100000LL; }
+long long bit21() { return 0x0000000000200000LL; }
+long long bit22() { return 0x0000000000400000LL; }
+long long bit23() { return 0x0000000000800000LL; }
+long long bit24() { return 0x0000000001000000LL; }
+long long bit25() { return 0x0000000002000000LL; }
+long long bit26() { return 0x0000000004000000LL; }
+long long bit27() { return 0x0000000008000000LL; }
+long long bit28() { return 0x0000000010000000LL; }
+long long bit29() { return 0x0000000020000000LL; }
+long long bit30() { return 0x0000000040000000LL; }
+long long bit31() { return 0x0000000080000000LL; }
+long long bit32() { return 0x0000000100000000LL; }
+long long bit33() { return 0x0000000200000000LL; }
+long long bit34() { return 0x0000000400000000LL; }
+long long bit35() { return 0x0000000800000000LL; }
+long long bit36() { return 0x0000001000000000LL; }
+long long bit37() { return 0x0000002000000000LL; }
+long long bit38() { return 0x0000004000000000LL; }
+long long bit39() { return 0x0000008000000000LL; }
+long long bit40() { return 0x0000010000000000LL; }
+long long bit41() { return 0x0000020000000000LL; }
+long long bit42() { return 0x0000040000000000LL; }
+long long bit43() { return 0x0000080000000000LL; }
+long long bit44() { return 0x0000100000000000LL; }
+long long bit45() { return 0x0000200000000000LL; }
+long long bit46() { return 0x0000400000000000LL; }
+long long bit47() { return 0x0000800000000000LL; }
+long long bit48() { return 0x0001000000000000LL; }
+long long bit49() { return 0x0002000000000000LL; }
+long long bit50() { return 0x0004000000000000LL; }
+long long bit51() { return 0x0008000000000000LL; }
+long long bit52() { return 0x0010000000000000LL; }
+long long bit53() { return 0x0020000000000000LL; }
+long long bit54() { return 0x0040000000000000LL; }
+long long bit55() { return 0x0080000000000000LL; }
+long long bit56() { return 0x0100000000000000LL; }
+long long bit57() { return 0x0200000000000000LL; }
+long long bit58() { return 0x0400000000000000LL; }
+long long bit59() { return 0x0800000000000000LL; }
+long long bit60() { return 0x1000000000000000LL; }
+long long bit61() { return 0x2000000000000000LL; }
+long long bit62() { return 0x4000000000000000LL; }
+long long bit63() { return 0x8000000000000000LL; }
+
+
 int main(int argc, const char *argv[]) {
   printf("%08x\n", zz());
   printf("%08x\n", zs());
@@ -184,6 +250,71 @@ int main(int argc, const char *argv[]) {
   printf("%016llx\n", uuuz());
   printf("%016llx\n", uuus());
   printf("%016llx\n", uuuu());
+
+  printf("%016llx\n", bit00());
+  printf("%016llx\n", bit01());
+  printf("%016llx\n", bit02());
+  printf("%016llx\n", bit03());
+  printf("%016llx\n", bit04());
+  printf("%016llx\n", bit05());
+  printf("%016llx\n", bit06());
+  printf("%016llx\n", bit07());
+  printf("%016llx\n", bit08());
+  printf("%016llx\n", bit09());
+  printf("%016llx\n", bit10());
+  printf("%016llx\n", bit11());
+  printf("%016llx\n", bit12());
+  printf("%016llx\n", bit13());
+  printf("%016llx\n", bit14());
+  printf("%016llx\n", bit15());
+  printf("%016llx\n", bit16());
+  printf("%016llx\n", bit17());
+  printf("%016llx\n", bit18());
+  printf("%016llx\n", bit19());
+  printf("%016llx\n", bit20());
+  printf("%016llx\n", bit21());
+  printf("%016llx\n", bit22());
+  printf("%016llx\n", bit23());
+  printf("%016llx\n", bit24());
+  printf("%016llx\n", bit25());
+  printf("%016llx\n", bit26());
+  printf("%016llx\n", bit27());
+  printf("%016llx\n", bit28());
+  printf("%016llx\n", bit29());
+  printf("%016llx\n", bit30());
+  printf("%016llx\n", bit31());
+  printf("%016llx\n", bit32());
+  printf("%016llx\n", bit33());
+  printf("%016llx\n", bit34());
+  printf("%016llx\n", bit35());
+  printf("%016llx\n", bit36());
+  printf("%016llx\n", bit37());
+  printf("%016llx\n", bit38());
+  printf("%016llx\n", bit39());
+  printf("%016llx\n", bit40());
+  printf("%016llx\n", bit41());
+  printf("%016llx\n", bit42());
+  printf("%016llx\n", bit43());
+  printf("%016llx\n", bit44());
+  printf("%016llx\n", bit45());
+  printf("%016llx\n", bit46());
+  printf("%016llx\n", bit47());
+  printf("%016llx\n", bit48());
+  printf("%016llx\n", bit49());
+  printf("%016llx\n", bit50());
+  printf("%016llx\n", bit51());
+  printf("%016llx\n", bit52());
+  printf("%016llx\n", bit53());
+  printf("%016llx\n", bit54());
+  printf("%016llx\n", bit55());
+  printf("%016llx\n", bit56());
+  printf("%016llx\n", bit57());
+  printf("%016llx\n", bit58());
+  printf("%016llx\n", bit59());
+  printf("%016llx\n", bit60());
+  printf("%016llx\n", bit61());
+  printf("%016llx\n", bit62());
+  printf("%016llx\n", bit63());
 
   return 0;
 }
