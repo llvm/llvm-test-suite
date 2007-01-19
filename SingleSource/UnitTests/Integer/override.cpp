255d306c4dbb9a7e307598b1fba1d0c394ee4500
Reid Spencer <rspencer@reidspencer.com>
2007-01-19 22:20:57 +0000

    Print something useful out.
    llvm-svn: 33376

--- SingleSource/UnitTests/Integer/override.cpp
+++ SingleSource/UnitTests/Integer/override.cpp
@@ -4,16 +4,17 @@ typedef int __attribute__ ((bitwidth(32))) int32;
 
 void func(int32 i)
 {
-    printf("call func with int32");
+    printf("call func with int32: %d\n", (int)i);
 }
 
 void func(int31 s)
 {
-    printf("call func with int31\n");
+    printf("call func with int31: %d\n", (int)s);
 }
 
 int main()
 {
     func( (int31) 1);
+    func( (int32) 2);
     return 0;
 }
