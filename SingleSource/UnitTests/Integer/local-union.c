579a58928c8edb3920fed7f1997c057f6bcb0e87
Reid Spencer <rspencer@reidspencer.com>
2007-01-22 21:04:54 +0000

    Fix constant integer for 64-bits. Fix header to 80 cols.
    llvm-svn: 33436

--- SingleSource/UnitTests/Integer/local-union.c
+++ SingleSource/UnitTests/Integer/local-union.c
@@ -1,13 +1,13 @@
-//===--- local-union.c --- Test Cases for Bit Accurate Types ------------===//
+//===--- local-union.c --- Test Cases for Bit Accurate Types --------------===//
 //
 // This file was developed by Guoling han and donated to the LLVM research
 // group and is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
-//===-------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // This is a test for local union data type.
 //
-//===--------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 #include <stdio.h>
 
@@ -18,7 +18,7 @@ union foo { int X; int33 Y;};
 double test(union foo* F) {
   {
     union foo { float X; int33 Y;} A;
-    A.Y = 0x1ffffffff;
+    A.Y = 0x1ffffffffULL;
     return A.X;
   }
 }
