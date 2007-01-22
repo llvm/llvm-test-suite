0b808a5c1986d7c271d7dfe176e6a3005c2e64f3
Guoling Han <han@autoesl.com>
2007-01-22 23:33:38 +0000

    Change convert.c to conform with the coding standard.
    llvm-svn: 33452

--- SingleSource/UnitTests/Integer/convert.c
+++ SingleSource/UnitTests/Integer/convert.c
@@ -1,11 +1,13 @@
-//===--- convert.c --- Test Cases for Bit Accurate Types --------------===////
-// This file was developed by Guoling han and donated to the LLVM research
-// group and is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
-//===----------------------------------------------------------------------===////
+//===--- convert.c --- Test Cases for Bit Accurate Types ------------------===//
+//
+// This file was developed by Guoling Han and is distributed under the 
+// University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
 // This is a test for conversion between different int types.
 //
 //===----------------------------------------------------------------------===//
+
 #include <stdio.h>
 
 typedef int __attribute__ ((bitwidth(7))) int7;
@@ -34,18 +36,18 @@ int main()
     i15 = (int15)i7;
     i31 = (int31)i15;
     if(i15 != -1 || i31 != -1)
-        printf("error: i15=%d, i31 = %d\n", i15, i31);
+      printf("error: i15=%d, i31 = %d\n", i15, i31);
 
     ui7 = 0x7f;
     ui15 = (uint15)ui7;
     ui31 = (uint31)ui15;
     if(ui15 != 0x7f || ui31 != 0x7f)
-        printf("error: ui15=%u, ui31 = %u\n", ui15, ui31);
+      printf("error: ui15=%u, ui31 = %u\n", ui15, ui31);
 
     i31 = -1;
     i8 = (int8) i31;
     if(i8 != -1)
-        printf("error: i8=%d\n", i8);
+      printf("error: i8=%d\n", i8);
 
     i31 = 0xff;
     i7 = (int7) i31;
