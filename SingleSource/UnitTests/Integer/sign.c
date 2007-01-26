fd8b0b8242c82d3b4bab795d590aa5fb7349ad69
Guoling Han <han@autoesl.com>
2007-01-26 23:34:47 +0000

    Current compiler failed to generate correct ll code
    llvm-svn: 33559

---
+++ SingleSource/UnitTests/Integer/sign.c
@@ -0,0 +1,63 @@
+//===--- sign.c --- Test Cases for Bit Accurate Types --------------------===//
+//
+// This file was developed by Guoling Han and is distributed under the 
+// University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This is a general test for arithmetic operations with signed and
+// unsigned operands.
+//
+//===----------------------------------------------------------------------===//
+
+#include <stdio.h>
+
+typedef int __attribute__ ((bitwidth(24))) int24;
+typedef unsigned int __attribute__ ((bitwidth(24))) uint24;
+
+
+int
+main ( int argc, char** argv)
+{
+  int num, r;
+  int24 x, y, z;
+  uint24 ux, uy, uz;
+
+  r = rand();
+  r = r - 1804289384; // -1
+
+  y = r; // -1
+  uy = r; // 0xffffff
+  
+  if (argc > 1)
+    num = atoi(argv[1]);
+
+  
+  num = num - 0xdf5e75; //0x1000001
+
+  x = num;
+  ux = num;
+  printf("x = %d, ux = %u, y=%d, uy = %u\n", x, ux, y, uy);
+    
+  z = x * y;  // 0x1000001 * (-1)
+  uz = ux * uy;
+  printf("z=%d, uz=%u\n", z, uz);
+
+  z = x % 314;
+  uz = ux % 314;
+  printf("z=%d, uz=%u\n", z, uz);
+
+  z = x / 314;
+  uz = ux / 314;
+  printf("z=%d, uz=%u\n", z, uz);
+
+  z = (x+0xf28) / 314;
+  uz = (ux + 0xf28) / 314;
+  printf("z=%d, uz=%u\n", z, uz);
+
+  z = (x - 580) / 314;
+  uz = (uint24)(((uint24)(ux - (uint24)580)) / (uint24)314);
+  printf("z=%d, uz=%u\n", z, uz);
+
+  return 0;
+}
