8b28f5930c34847dbfc530df1e785cef07145f72
Chris Lattner <sabre@nondot.org>
2007-05-13 22:00:41 +0000

    darwinize this testcase
    llvm-svn: 37022

--- SingleSource/UnitTests/2007-04-25-weak.c
+++ SingleSource/UnitTests/2007-04-25-weak.c
@@ -1,4 +1,9 @@
+
+#if !defined(__linux__) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
+extern int test_weak () __attribute__ ((weak_import));
+#else
 extern int test_weak () __attribute__ ((weak));
+#endif
 
 int main(){
   int (*t)() = test_weak;
