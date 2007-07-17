16bfaee62fa1ad6dab66c298afeacf40a739717b
Reid Spencer <rspencer@reidspencer.com>
2007-07-17 20:11:11 +0000

    Update this test to show the output of the thing it is testing.
    llvm-svn: 39981

--- SingleSource/UnitTests/Integer/integer_all_onesp.c
+++ SingleSource/UnitTests/Integer/integer_all_onesp.c
@@ -3,7 +3,11 @@
 int main(int argc, char **argv) {
   typedef uint256 BitType;
 
-  BitType X = rand() % bitwidthof(BitType);
+  BitType X = -1;
 
-  return 0;
+  printf("uint256 X = -1 -> ");
+  printBits(X);
+  printf("\n");
+
+  return (int) X;
 }
