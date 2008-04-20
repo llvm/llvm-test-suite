d2f50651ad3ce6fca4cc662d856e49d8316a2ee6
Dale Johannesen <dalej@apple.com>
2008-04-20 17:00:14 +0000

    New test.  Pedantic changes to previous test.
    llvm-svn: 49992

--- SingleSource/UnitTests/2008-04-20-LoopBug2.c
+++ SingleSource/UnitTests/2008-04-20-LoopBug2.c
@@ -1,9 +1,11 @@
 extern int printf(const char*, ...);
 void foo( unsigned int i, int array[4]) __attribute__((noinline));
 void foo( unsigned int i, int array[4]) {
-  unsigned int j;
-  for (j=3; j>i; j--)
+  unsigned int j=3;
+  do {
     array[j] = array[j-1];
+    j--;
+  }  while (j>i);
   printf("%d %d %d %d %d\n", i, array[0], array[1], array[2], array[3]);
 }
 main() {
@@ -13,6 +15,6 @@ main() {
     foo(i, array);
   }
   array[0] = 5; array[1] = 6; array[2] = 7; array[3] = 8;
-  foo(0xffffffffu, array);
+  foo(0xffffffffU, array);
   return 0;
 }
