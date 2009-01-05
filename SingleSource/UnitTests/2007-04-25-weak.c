e718898c40df562cf82a32a92c9cf6a809a7dac0
Dan Gohman <gohman@apple.com>
2009-01-05 04:55:51 +0000

    Fix this test so that main returns 0 on success.
    llvm-svn: 61673

--- SingleSource/UnitTests/2007-04-25-weak.c
+++ SingleSource/UnitTests/2007-04-25-weak.c
@@ -7,7 +7,9 @@ extern int test_weak () __attribute__ ((weak));
 
 int main(){
   int (*t)() = test_weak;
-  if (t)
-    return t();
-  else return 250;
+  if (t) {
+    t();
+    return 1;
+  }
+  return 0;
 }
