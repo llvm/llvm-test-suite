b14b13529d31e4613cd1f6a42c06156031d6329e
Evan Cheng <evan.cheng@apple.com>
2009-12-18 07:48:44 +0000

    Add a small test case to show the benefit of not folding load into cvtss2sd.
    llvm-svn: 91673

---
+++ SingleSource/Benchmarks/Misc/fp-convert.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double loop(float *x, float *y, long length) {
+  long i;
+  double accumulator = 0.0;
+  for (i=0; i<length; ++i) {
+    accumulator += (double)x[i] * (double)y[i];
+  }
+  return accumulator;
+}
+
+int main(int argc, char *argv[]) {
+  int i, j;
+  float x[2048];
+  float y[2048];
+  double total = 0.0;
+  float a = 0.0f;
+  float b = 1.0f;
+    
+  for (i=0; i<500000; ++i) {
+    if (i % 10) {
+      a = 0.0f;
+      b = 1.0f;
+    } else {
+      a += 0.1f;
+      b += 0.2f;
+    }
+    for (j=0; j<2048; ++j) {
+      x[j] = a + (float)j;
+      y[j] = b + (float)j;
+    }
+    total += loop(x, y, 2048);
+  }
+  
+  printf("Total is %g\n", total);
+    
+  return 0;
+}
