e06779fc9d33bdc840d39d0cd769e6a2b741a0de
Daniel Dunbar <daniel@zuster.org>
2010-02-23 03:20:27 +0000

    Rewrite test in a C99 compliant way.
    llvm-svn: 96878

--- SingleSource/Regression/C/2003-05-21-UnionBitfields.c
+++ SingleSource/Regression/C/2003-05-21-UnionBitfields.c
@@ -17,6 +17,6 @@ int target_isinf(double x) {
 }
 
 int main() {
-  printf("%d %d\n", target_isinf(1234.42), target_isinf(1.0/1.0e-1000));
+  printf("%d %d\n", target_isinf(1234.42), target_isinf(1.0/5.0e-324));
   return 0;
 }
