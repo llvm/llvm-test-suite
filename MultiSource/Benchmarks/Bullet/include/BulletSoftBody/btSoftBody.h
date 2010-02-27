a2548b1531158a202bc4f94aa8df622f70f5f0e4
Douglas Gregor <dgregor@apple.com>
2010-02-27 19:14:19 +0000

    Add some missing user-defined constructors, which Clang (rightfully) complains about.
    llvm-svn: 97337

--- MultiSource/Benchmarks/Bullet/include/BulletSoftBody/btSoftBody.h
+++ MultiSource/Benchmarks/Bullet/include/BulletSoftBody/btSoftBody.h
@@ -182,6 +182,8 @@ public:
 		btScalar				m_kAST;			// Area/Angular stiffness coefficient [0,1]
 		btScalar				m_kVST;			// Volume stiffness coefficient [0,1]
 		int						m_flags;		// Flags
+
+                Material() { }
 	};
 
 	/* Feature		*/ 
@@ -201,6 +203,8 @@ public:
 		btScalar				m_area;			// Area
 		btDbvtNode*				m_leaf;			// Leaf data
 		int						m_battach:1;	// Attached
+
+                Node() { }
 	};
 	/* Link			*/ 
 	struct	Link : Feature
@@ -212,6 +216,8 @@ public:
 		btScalar				m_c1;			// rl^2
 		btScalar				m_c2;			// |gradient|^2/c0
 		btVector3				m_c3;			// gradient
+
+                Link() { }
 	};
 	/* Face			*/ 
 	struct	Face : Feature
@@ -220,6 +226,8 @@ public:
 		btVector3				m_normal;		// Normal
 		btScalar				m_ra;			// Rest area
 		btDbvtNode*				m_leaf;			// Leaf data
+
+                Face() { }
 	};
 	/* Tetra		*/ 
 	struct	Tetra : Feature
@@ -230,6 +238,8 @@ public:
 		btVector3				m_c0[4];		// gradients
 		btScalar				m_c1;			// (4*kVST)/(im0+im1+im2+im3)
 		btScalar				m_c2;			// m_c1/sum(|g0..3|^2)
+
+                Tetra() { }
 	};
 	/* RContact		*/ 
 	struct	RContact
@@ -271,6 +281,7 @@ public:
 		int						m_rank;			// Rank
 		Node*					m_nodes[4];		// Nodes
 		btScalar				m_coords[4];	// Coordinates
+                Note() { }
 	};	
 	/* Pose			*/ 
 	struct	Pose
