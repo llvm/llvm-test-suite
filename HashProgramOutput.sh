ef17be3e97399688e6ab8f8870678b06731adac4
Daniel Dunbar <daniel@zuster.org>
2010-05-30 00:48:18 +0000

    Add HASH_PROGRAM_OUTPUT makefile variable, which uses md5sum/md5 to hash the program output before comparing. This is useful for tests which have ridiculously large expected output files (as in, tens of MBs).  - Currently only enabled with ENABLE_HASHED_PROGRAM_OUTPUT=1.
    llvm-svn: 105125

---
+++ HashProgramOutput.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+  echo "$0 <output path>"
+  exit 1
+fi
+
+md5cmd=$(which md5sum)
+if [ ! -x "$md5cmd" ]; then
+    md5cmd=$(which md5)
+    if [ ! -x "$md5cmd" ]; then
+        echo "error: unable to find either 'md5sum' or 'md5'"
+        exit 1
+    fi
+fi
+
+mv $1 $1.bak
+md5 < $1.bak > $1
+rm -f $1.bak
