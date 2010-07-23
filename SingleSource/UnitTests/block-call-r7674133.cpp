10635df1221af7ae0793bdcfd6d0ab71dd3edd8c
Fariborz Jahanian <fjahanian@apple.com>
2010-07-23 21:01:54 +0000

    Block test for clang produced executable. This is test case for radar 7674133.
    llvm-svn: 109264

---
+++ SingleSource/UnitTests/block-call-r7674133.cpp
@@ -0,0 +1,36 @@
+#ifdef __clang__
+extern "C" void abort(void);
+
+class Test {
+public:
+    int i;
+    Test() : i(0)
+    {
+    }
+    
+    ~Test()
+    {
+	i = -1;;
+    }
+    
+    Test(const Test&r)
+    {
+	i = r.i;
+    }
+    
+    void speak() const
+    {
+	if (i != 0)
+	  abort();
+    }
+};
+#endif
+
+int main (int , char * const []) 
+{
+#ifdef __clang__
+    Test t;
+    ^{  t.speak();  }();
+#endif
+    return 0;
+}    
