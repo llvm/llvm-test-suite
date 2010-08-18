b5b8fff00170c8f6f89de723d57c6485e0734612
Daniel Dunbar <daniel@zuster.org>
2010-08-18 23:43:47 +0000

    HashProgramOutput: Filter md5sum output to match expected format.
    llvm-svn: 111460

--- HashProgramOutput.sh
+++ HashProgramOutput.sh
@@ -6,8 +6,10 @@ if [ $# != 1 ]; then
 fi
 
 md5cmd=$(which md5sum)
+is_md5sum=1
 if [ ! -x "$md5cmd" ]; then
     md5cmd=$(which md5)
+    is_md5sum=0
     if [ ! -x "$md5cmd" ]; then
         echo "error: unable to find either 'md5sum' or 'md5'"
         exit 1
@@ -15,5 +17,9 @@ if [ ! -x "$md5cmd" ]; then
 fi
 
 mv $1 $1.bak
-$md5cmd < $1.bak > $1
+if [ $is_md5sum = "0" ]; then
+    $md5cmd < $1.bak > $1
+else
+    $md5cmd < $1.bak | cut -d' ' -f 1 > $1
+fi
 rm -f $1.bak
