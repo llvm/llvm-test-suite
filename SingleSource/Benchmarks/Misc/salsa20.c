5cc119b07db873a60ee34243f38d72d7161e4d1e
Daniel Dunbar <daniel@zuster.org>
2010-10-19 22:30:36 +0000

    Fix test to honor SMALL_PROBLEM_SIZE, this test is stupid.
    llvm-svn: 116872

--- SingleSource/Benchmarks/Misc/salsa20.c
+++ SingleSource/Benchmarks/Misc/salsa20.c
@@ -51,14 +51,24 @@ uint32_t salsa(void) {
 
 int main(void) {
   uint32_t val, i;
+#ifdef SMALL_PROBLEM_SIZE
+  uint32_t count = 5379194;
+  uint32_t offset = 0xf655703d - 0xb67e7950;
+#else
+  uint32_t count = 537919488;
+  uint32_t offset = 0;
+#endif
 
   for(i=0; i<16; i++)
     STATE[i] = (0xedababe5+(i+13))^(0xdeadbeef-i);
   
-  for(i=0; i<537919488; i++)
+  for(i=0; i<count; i++)
     salsa();
 
-  printf("got:       %x\n", val = salsa());
+  val = salsa();
+  val += offset;
+
+  printf("got:       %x\n", val);
   printf("should be: %x\n", 0xf655703d);
   return(val != 0xf655703d);
 }
