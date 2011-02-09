0230324cfebc50850ee489f3884e8b907e4fe2a4
Daniel Dunbar <daniel@zuster.org>
2011-02-09 00:44:15 +0000

    test-suite: Start sketching some tools.  - timeit is just a simple replacement for /usr/bin/time which prints a whopping    four digits of precision. That's right, FOUR!  - fpcmp is just stubbed out at the moment. Yes, I will be reimplementing fpcmp    in C.
    llvm-svn: 125147

---
+++ tools/fpcmp.c
@@ -0,0 +1,14 @@
+/*===-- timeit.c - LLVM Test Suite Timing Tool ------------------*- C++ -*-===*\
+|*                                                                            *|
+|*                     The LLVM Compiler Infrastructure                       *|
+|*                                                                            *|
+|* This file is distributed under the University of Illinois Open Source      *|
+|* License. See LICENSE.TXT for details.                                      *|
+|*                                                                            *|
+\*===----------------------------------------------------------------------===*/
+
+#include <stdio.h>
+
+int main(int argc, char * const argv[]) {
+  return 0;
+}
