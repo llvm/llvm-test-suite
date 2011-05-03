9428ef41cce48e0caab4e4fa6f0b85fa47abe3a2
Fariborz Jahanian <fjahanian@apple.com>
2011-05-03 22:17:46 +0000

    More unit tests for ms_struct and zero-length bitfield alignment rules.
    llvm-svn: 130798

--- SingleSource/UnitTests/ms_struct-bitfield-init-1.c
+++ SingleSource/UnitTests/ms_struct-bitfield-init-1.c
@@ -42,6 +42,44 @@ struct
 } ATTR t5 = {7,8};
 static int a5[(sizeof(t5) == 4) -1];
 
+struct
+{
+                   char foo : 4;
+                   short : 0;
+                   long  :0;
+                   char bar;
+} ATTR t6 = {5, 10};
+static int a6[(sizeof(t6) == 4) -1];
+
+struct
+{
+                   char foo : 4;
+                   int  :0;
+                   short : 0;
+                   char bar;
+} ATTR t7 = {5, 10};
+static int a7[(sizeof(t7) == 8) -1];
+
+struct
+{
+                   char foo : 4;
+                   short : 0;
+                   int  :0;
+                   char bar:7;
+} ATTR t8 = {5, 10};
+static int a8[(sizeof(t8) == 4) -1];
+
+struct
+{
+                   char foo : 4;
+                   char : 0;
+                   short : 0;
+                   int : 0;
+                   long  :0;
+                   char bar;
+} ATTR t10 = {5, 10};
+static int a10[(sizeof(t10) == 2) -1];
+
 int main() {
   if (t1.bf_1 != 1 || t1.bf_2 != 2)
     abort();
@@ -50,6 +88,14 @@ int main() {
   if (t3.foo != 5 || t3.bar != 6)
     abort();
   if (t5.foo != 7 || t5.bar != 8)
+    abort();
+  if (t6.foo != 5 || t6.bar != 10)
+    abort();
+  if (t7.foo != 5 || t7.bar != 10)
+    abort();
+  if (t8.foo != 5 || t8.bar != 10)
+    abort();
+  if (t10.foo != 5 || t10.bar != 10)
     abort();
   return 0;
 }
