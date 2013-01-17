52aec3d6f233e02dc2ee53f2c64c0ecfd30bf8a2
Tim Northover <Tim.Northover@arm.com>
2013-01-17 13:24:38 +0000

    Explicitly specify "signed char" when testing extensions.
    The ARM ABIs specify that char is unsigned by default, in contrast to many
    others. This test seemed more concerned about the extensions that get applied
    to "signed char" than whether that's the default so just add "signed" to the
    types.
    
    llvm-svn: 172716

--- SingleSource/UnitTests/2003-07-10-SignConversions.c
+++ SingleSource/UnitTests/2003-07-10-SignConversions.c
@@ -6,27 +6,27 @@
 
 unsigned char getUC() { return 0x80; }
 
-         char getC()  { return 0x80; }
+signed   char getSC() { return 0x80; }
 
 int main()
 {
-  char            C80 = getC();
+  signed   char  SC80 = getSC();
   unsigned char  UC80 = getUC();
 
   /* source is smaller than dest: both decide */
-  unsigned short us  = (unsigned short)  C80;     /* sign-ext then zero-ext */
+  unsigned short us  = (unsigned short) SC80;     /* sign-ext then zero-ext */
   unsigned short us2 = (unsigned short) UC80;     /* zero-ext only: NOP! */
-           short  s  = (         short)  C80;     /* sign-ext */ 
+           short  s  = (         short) SC80;     /* sign-ext */
            short  s2 = (         short) UC80;     /* zero-extend only : NOP! */
-  printf("%d %d --> unsigned: us = %d, us2 = %d\n", C80, UC80, us, us2);
-  printf("%d %d -->   signed:  s = %d,  s2 = %d\n", C80, UC80,  s,  s2);
+  printf("%d %d --> unsigned: us = %d, us2 = %d\n", SC80, UC80, us, us2);
+  printf("%d %d -->   signed:  s = %d,  s2 = %d\n", SC80, UC80,  s,  s2);
 
   /* source is same size or larger than dest: dest decides */
-  unsigned char  uc  = (unsigned char )  C80;     /* zero-ext */
+  unsigned char  uc  = (unsigned char ) SC80;     /* zero-ext */
   unsigned char  uc2 = (unsigned char ) UC80;     /* NOP */
-           char   c  = (         char )  C80;     /* NOP */
-           char   c2 = (         char ) UC80;     /* sign-extend */
-  printf("%d %d --> unsigned: uc = %d, uc2 = %d\n", C80, UC80, uc, uc2);
-  printf("%d %d -->   signed:  c = %d,  c2 = %d\n", C80, UC80,  c,  c2);
+  signed   char  sc  = (signed   char ) SC80;     /* NOP */
+  signed   char  sc2 = (signed   char ) UC80;     /* sign-extend */
+  printf("%d %d --> unsigned: uc = %d, uc2 = %d\n", SC80, UC80, uc, uc2);
+  printf("%d %d -->   signed: sc = %d, sc2 = %d\n", SC80, UC80, sc, sc2);
   return 0;
 }
