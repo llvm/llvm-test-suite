14768b18f1ff969ed9378329968c4578183697c0
Renato Golin <renato.golin@linaro.org>
2013-04-02 11:47:17 +0000

    Try to make anagram's sort more stable
    The reference_output did not match the output of the
    program, neither with current comparator, not with
    the one changed in this patch. Though, the new version
    should be more stable, and possibly produce the same
    order in different library implementations.
    
    If not, it's quite possible that the problem is not
    in the comparator per se, but in how auGlobalFrequency
    is built, which will warrant a deeper investigation.
    
    llvm-svn: 178535

--- MultiSource/Benchmarks/Ptrdist/anagram/anagram.c
+++ MultiSource/Benchmarks/Ptrdist/anagram/anagram.c
@@ -578,10 +578,15 @@ FindAnagram(Quad * pqMask, PPWord ppwStart, int iLetter)
 }
 
 int Cdecl CompareFrequency(char *pch1, char *pch2) {
-    return auGlobalFrequency[*pch1] < auGlobalFrequency[*pch2]
-        ?  -1 :
-           auGlobalFrequency[*pch1] == auGlobalFrequency[*pch2]
-        ?   0 : 1;
+    if (auGlobalFrequency[*pch1] < auGlobalFrequency[*pch2])
+        return -1;
+	if (auGlobalFrequency[*pch1] > auGlobalFrequency[*pch2])
+        return 1;
+    if (*pch1 < *pch2)
+        return -1;
+	if (*pch1 > *pch2)
+        return 1;
+    return 0;
 }
 
 void SortCandidates(void) {
