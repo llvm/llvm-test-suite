c9b8210aaadc424642bef075a4153f40dd6ea53d
Bill Wendling <isanbard@gmail.com>
2013-04-03 00:04:26 +0000

    Add test for atomic addition and subtraction. <rdar://problem/13549656>
    llvm-svn: 178594

--- SingleSource/UnitTests/AtomicOps.c
+++ SingleSource/UnitTests/AtomicOps.c
@@ -7,11 +7,24 @@ int foo(volatile *mem, int val, int c) {
 
 volatile int x = 0;
 int main() {
+  long long test = 0;
+  int i;
   int y = foo(&x, 1, 2);
   printf("%d, %d\n", y, x);
   y = __sync_val_compare_and_swap(&x, 1, 2);
   printf("%d, %d\n", y, x);
   y = __sync_lock_test_and_set(&x, 1);
   printf("%d, %d\n", y, x);
+
+  for (i = 0; i < 5; i++) {
+    __sync_add_and_fetch(&test, 1);
+    printf("test = %d\n", (int)test);
+  }
+
+  for (i = 0; i < 5; i++) {
+    __sync_fetch_and_sub(&test,1);
+    printf("test = %d\n", (int)test);
+  }
+
   return 0;
 }
