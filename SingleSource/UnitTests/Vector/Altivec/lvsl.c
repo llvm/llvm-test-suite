97b94b44b69e276c13ab67dbcd153763daa7a78d
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2014-10-07 13:32:05 +0000

    [PowerPC] Fix lvsr.c and lvsl.c tests
    The new Altivec tests I added to projects/test-suite yesterday
    contained incorrect checking of the results.  I apparently checked the
    wrong log file during testing and missed this.  /shamefaced
    
    Now the tests actually work...
    
    llvm-svn: 219205

--- SingleSource/UnitTests/Vector/Altivec/lvsl.c
+++ SingleSource/UnitTests/Vector/Altivec/lvsl.c
@@ -10,11 +10,11 @@ __attribute__((__aligned__(16))) float f[16];
 
 static void test() {
   vector unsigned char expected = {4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
-  check (vec_lvsl(0, &sc[4]) == expected);
-  check (vec_lvsl(0, &uc[4]) == expected);
-  check (vec_lvsl(0, &ss[2]) == expected);
-  check (vec_lvsl(0, &us[2]) == expected);
-  check (vec_lvsl(0, &si[1]) == expected);
-  check (vec_lvsl(0, &ui[1]) == expected);
-  check (vec_lvsl(0, & f[1]) == expected);
+  check (vec_all_eq(vec_lvsl(0, &sc[4]), expected), "sc");
+  check (vec_all_eq(vec_lvsl(0, &uc[4]), expected), "uc");
+  check (vec_all_eq(vec_lvsl(0, &ss[2]), expected), "ss");
+  check (vec_all_eq(vec_lvsl(0, &us[2]), expected), "us");
+  check (vec_all_eq(vec_lvsl(0, &si[1]), expected), "si");
+  check (vec_all_eq(vec_lvsl(0, &ui[1]), expected), "ui");
+  check (vec_all_eq(vec_lvsl(0, & f[1]), expected), "f");
 }
--- SingleSource/UnitTests/Vector/Altivec/lvsr.c
+++ SingleSource/UnitTests/Vector/Altivec/lvsr.c
@@ -11,11 +11,11 @@ __attribute__((__aligned__(16))) float f[16];
 static void test() {
   vector unsigned char expected = {12,13,14,15,16,17,18,19,
                                    20,21,22,23,24,25,26,27};
-  check (vec_lvsr(0, &sc[4]) == expected);
-  check (vec_lvsr(0, &uc[4]) == expected);
-  check (vec_lvsr(0, &ss[2]) == expected);
-  check (vec_lvsr(0, &us[2]) == expected);
-  check (vec_lvsr(0, &si[1]) == expected);
-  check (vec_lvsr(0, &ui[1]) == expected);
-  check (vec_lvsr(0, & f[1]) == expected);
+  check (vec_all_eq(vec_lvsr(0, &sc[4]), expected), "sc");
+  check (vec_all_eq(vec_lvsr(0, &uc[4]), expected), "uc");
+  check (vec_all_eq(vec_lvsr(0, &ss[2]), expected), "ss");
+  check (vec_all_eq(vec_lvsr(0, &us[2]), expected), "us");
+  check (vec_all_eq(vec_lvsr(0, &si[1]), expected), "si");
+  check (vec_all_eq(vec_lvsr(0, &ui[1]), expected), "ui");
+  check (vec_all_eq(vec_lvsr(0, & f[1]), expected), "f");
 }
