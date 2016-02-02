399ecd7d1555dc2db33ba85a4570fb80f2d9f7ff
Matthias Braun <matze@braunis.de>
2016-02-02 21:47:41 +0000

    CINT2000/252.eon: Make benchmark compatible with modern C++ libs
    The benchmark included fstream.h and iostream.h which are ancient names
    not available anymore in modern C++ libs. Provide stubs for those two
    headers that just include fstream and iostream.
    
    llvm-svn: 259581

---
+++ External/SPEC/CINT2000/252.eon/fstream.h
@@ -0,0 +1,3 @@
+// Compatibility kludge because the benchmark is using ancient header names.
+#include <fstream>
+using namespace std;
---
+++ External/SPEC/CINT2000/252.eon/iostream.h
@@ -0,0 +1,3 @@
+// Compatibility kludge because the benchmark is using ancient header names.
+#include <iostream>
+using namespace std;
