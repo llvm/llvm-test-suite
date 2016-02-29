12b18088aa482834ca4a06acea8639e69fb1ad88
James Molloy <james.molloy@arm.com>
2016-02-29 14:16:38 +0000

    [cmake] hash support: make sure we run strip
    Stripping out certain sections of the binary under test is important to get a stable hash, and is an aspect of the Makefile-based hashing system that I was ignorant of.
    
    Implement this for LIT.
    
    llvm-svn: 262221

--- litsupport/hash.py
+++ litsupport/hash.py
@@ -1,13 +1,25 @@
 import lit.Test
 import hashlib
 import logging
+import subprocess
+import shutil
+import platform
 
 def collect(context, result):
     try:
         exename = context.test.getFilePath().rsplit('.test', 1)[0]
+        backup_exename = exename + '.stripped'
+        shutil.copyfile(exename, backup_exename)
+        
+        # Darwin's "strip" doesn't support these arguments.
+        if platform.system() != 'Darwin':
+            subprocess.check_call(['strip',
+                                   '--remove-section=.comment',
+                                   '--remove-section=.note',
+                                   backup_exename])
 
         h = hashlib.md5()
-        h.update(open(exename).read())
+        h.update(open(backup_exename).read())
         digest = h.hexdigest()
 
         result.addMetric('hash', lit.Test.toMetricValue(digest))
