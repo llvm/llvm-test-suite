a9b0341e7327389d8ed5a61a0e7b6c62e2d3d1d6
Matthias Braun <matze@braunis.de>
2016-03-14 18:12:36 +0000

    cmake/caches: Add some comments, more aggressive release mode
    llvm-svn: 263456

--- cmake/caches/Release.cmake
+++ cmake/caches/Release.cmake
@@ -1,4 +1,9 @@
-set(CMAKE_C_FLAGS_RELEASE "-O3 -flto" CACHE STRING "")
-set(CMAKE_CXX_FLAGS_RELEASE "-O3 -flto" CACHE STRING "")
+# Aggressive settings to produce fast code.
+set(OPTFLAGS "${OPTFLAGS} -O3 -march=native -fomit-frame-pointer -flto -DNDEBUG")
+if(APPLE)
+  set(OPTFLAGS "${OPTFLAGS} -mdynamic-no-pic")
+endif()
 
-set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
+set(CMAKE_C_FLAGS_RELEASE "${OPTFLAGS}" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELEASE "${OPTFLAGS}" CACHE STRING "")
+set(CMAKE_BUILD_TYPE "Release")
---
+++ cmake/caches/Release_Haswell.cmake
@@ -0,0 +1,2 @@
+set(OPTFLAGS "${OPTFLAGS} -march=haswell")
+include(${CMAKE_CURRENT_LIST_DIR}/Release.cmake)
