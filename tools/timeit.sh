089b03425a94a379eafa2da8e208d7b0461c2d0d
Matthias Braun <matze@braunis.de>
2016-03-14 21:14:56 +0000

    tools/timeit.sh: Fix space/tab inconsistencies
    Use an llvm style 2-space indentation.
    
    llvm-svn: 263492

--- tools/timeit.sh
+++ tools/timeit.sh
@@ -8,27 +8,27 @@ ERRPUT=/dev/stderr
 PERFSTAT=perfstats
 
 while [[ $1 = -* ]]; do
-	if [ $1 = "--summary" ]; then
-		REPORT=$2
-		PERFSTAT="$REPORT.perfstats"
-	elif [ $1 = "--redirect-input" ]; then
-		INPUT=$2
-	elif [ $1 = "--redirect-output" ]; then
-     	    OUTPUT=$2
-	    ERRPUT=$2
-	elif [ $1 = "--redirect-stdout" ]; then
-            OUTPUT=$2
-	elif [ $1 = "--chdir" ]; then
-            cd $2
-	fi
-	shift 2
+  if [ $1 = "--summary" ]; then
+    REPORT=$2
+    PERFSTAT="$REPORT.perfstats"
+  elif [ $1 = "--redirect-input" ]; then
+    INPUT=$2
+  elif [ $1 = "--redirect-output" ]; then
+    OUTPUT=$2
+    ERRPUT=$2
+  elif [ $1 = "--redirect-stdout" ]; then
+    OUTPUT=$2
+  elif [ $1 = "--chdir" ]; then
+    cd $2
+  fi
+  shift 2
 done
 
 if [ "$OUTPUT" = "$ERRPUT" ]; then
-    # Use >& to ensure the streams are properly interleaved.
-    perf stat -o $PERFSTAT $@ < $INPUT >& $OUTPUT
+  # Use >& to ensure the streams are properly interleaved.
+  perf stat -o $PERFSTAT $@ < $INPUT >& $OUTPUT
 else
-    perf stat -o $PERFSTAT $@ < $INPUT > $OUTPUT 2> $ERRPUT
+  perf stat -o $PERFSTAT $@ < $INPUT > $OUTPUT 2> $ERRPUT
 fi
 
 EXITCODE=$?
