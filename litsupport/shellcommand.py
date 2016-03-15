9e2fd679cf5e7c77a796a3cc8124b09d0f0b8166
Matthias Braun <matze@braunis.de>
2016-03-15 05:12:18 +0000

    litsupport/shellcommand: Add toCommandline() and wrap() convenience functions, support envvars
    llvm-svn: 263531

--- litsupport/shellcommand.py
+++ litsupport/shellcommand.py
@@ -1,24 +1,62 @@
 import shlex
 import logging
+import re
+try:
+        from shlex import quote  # python 3.3 and above
+except:
+        from pipes import quote  # python 3.2 and earlier
 
-# Loosely modeled after posix specification for sh
+
+class ShellCommand:
+    '''This class represents a parsed shell command (a subset, we do not support
+    arbitrary shell commands yet). The parsed form allows direct access to
+    information like the executable name, working directory or the file
+    to which stdin/stdout/stderr is redirected.'''
+    def __init__(self, executable=None, arguments=None):
+        self.stdin = None
+        self.stdout = None
+        self.stderr = None
+        self.executable = executable
+        if arguments is None:
+            arguments = []
+        self.arguments = arguments
+        self.workdir = None
+        self.envvars = {}
+
+    def toCommandline(self):
+        result = ""
+
+        if self.workdir is not None:
+            result += "cd %s && " % quote(self.workdir)
+
+        res_list = [self.executable] + self.arguments
+        result += " ".join(map(quote, res_list))
+
+        envlist = []
+        for key, value in self.envvars.items():
+            result += "%s=%s " % (key, quote(value))
+
+        if self.stdin is not None:
+            result += " < %s" % quote(self.stdin)
+        if self.stdout is not None:
+            result += " > %s" % quote(self.stdout)
+        if self.stderr is not None:
+            result += " 2> %s" % quote(self.stderr)
+        return result
+
+    def wrap(self, new_executable, args):
+        self.arguments = args + [self.executable] + self.arguments
+        self.executable = new_executable
+
+
+# Tokens/keywords for shell command parsing, loosely modeled after posix
+# specification for sh.
 reserved_words = ['!', '{', '}', 'case', 'do', 'done', 'elif', 'else', 'esac',
                   'fi', 'for', 'if', 'in', 'then', 'until', 'while']
 
 chaining_tokens = set([';', '&&', '||', '|', '&'])
 unhandled_tokens = set([';;', '<<', '>>', '<&', '>&', '<>', '<<-', '>|', '(',
                         ')'])
-
-
-class ShellCommand:
-    def __init__(self):
-        self.stdin = None
-        self.stdout = None
-        self.stderr = None
-        self.executable = None
-        self.arguments = []
-        self.modifiers = None
-        self.workdir = None
 
 
 def parse(commandline):
@@ -42,6 +80,11 @@ def parse(commandline):
             result.stderr = tokens[i]
             i += 1
             continue
+        assignment=re.match('([A-Za-z_][A-Za-z_0-9]*)=(.*)', token)
+        if assignment:
+            result.envvars[m.group(1)] = m.group(2)
+            continue
+
         if first_word:
             if token in reserved_words or token in unhandled_tokens:
                 raise Exception("Reserved word '%s' not supported" % token)
@@ -50,8 +93,9 @@ def parse(commandline):
         else:
             if token in chaining_tokens:
                 # Currently we only allow the special case of 'cd DIR ;'
-                if token == ';' and result.executable == 'cd' and \
-                      len(result.arguments) == 1:
+                if (token == ';' or token == '&&') and \
+                        result.executable == 'cd' and \
+                        len(result.arguments) == 1:
                     newresult = ShellCommand()
                     newresult.workdir = result.arguments[0]
                     result = newresult
