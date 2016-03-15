e0cac2842d83ed9edd8599d30635dc4ab636cb35
Matthias Braun <matze@braunis.de>
2016-03-15 05:24:03 +0000

    cmake/lit: Collect link_times
    Previously we only collected compile_times, this adds a new metric
    link_time. This currently has the same problem as compile_time with the
    SingleSource/** directories.
    
    llvm-svn: 263535

--- litsupport/compiletime.py
+++ litsupport/compiletime.py
@@ -7,13 +7,20 @@ def _getCompileTime(context):
     # TODO: This is not correct yet as the directory may contain .o.time files
     # of multiple benchmarks in the case of SingleSource tests.
     compile_time = 0.0
+    link_time = 0.0
     basepath = os.path.dirname(context.test.getFilePath())
     for path, subdirs, files in os.walk(basepath):
         for file in files:
             if file.endswith('.o.time'):
                 fullpath = os.path.join(path, file)
                 compile_time += timeit.getUserTime(fullpath)
-    return {'compile_time': lit.Test.toMetricValue(compile_time)}
+            if file.endswith('.link.time'):
+                fullpath = os.path.join(path, file)
+                link_time += timeit.getUserTime(fullpath)
+    return {
+        'compile_time': lit.Test.toMetricValue(compile_time),
+        'link_time': lit.Test.toMetricValue(link_time),
+    }
 
 
 def mutatePlan(context, plan):
