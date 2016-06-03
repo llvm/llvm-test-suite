28c22aef5ee4a801b92f6ddd4759be9b482ebdaa
Silviu Baranga <silviu.baranga@arm.com>
2016-06-03 11:35:05 +0000

    [test-suite] Use shellcommand.wrap() when adding the run_under command
    Summary:
    When mutating a command like "cd X; ./cmd" to add the run_under
    arguments (for example "taskset -c 1"), the mutated command needs to
    maintain the "cd X;" part in the output command prefix:
       "cd X; taskset -c 1 ./cmd"
    
    If we don't do this, the mutated command cannot be parsed.
    
    Use shellcommand.wrap() to mutate the command instead of string
    concatenation, since shellcommand knows how to handle the
    "cd X; ..".
    
    Reviewers: MatzeB
    
    Subscribers: llvm-commits
    
    Differential Revision: http://reviews.llvm.org/D20854
    
    llvm-svn: 271661

--- litsupport/run_under.py
+++ litsupport/run_under.py
@@ -3,7 +3,19 @@ from litsupport import testplan
 
 
 def mutateCommandLine(context, commandline):
-    return context.config.run_under + " " + commandline
+    cmd = shellcommand.parse(commandline)
+    run_under_cmd = shellcommand.parse(context.config.run_under)
+
+    if (run_under_cmd.stdin is not None or
+        run_under_cmd.stdout is not None or
+        run_under_cmd.stderr is not None or
+        run_under_cmd.workdir is not None or
+        run_under_cmd.envvars):
+        raise Exception("invalid run_under argument!")
+
+    cmd.wrap(run_under_cmd.executable, run_under_cmd.arguments)
+
+    return cmd.toCommandline()
 
 
 def mutatePlan(context, plan):
