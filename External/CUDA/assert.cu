14461c75238a361a314271562d714ddfb37b0291
Justin Lebar <jlebar@google.com>
2016-09-16 04:07:35 +0000

    [CUDA] [test-suite] Add test that assert(false)'s.
    This is mostly so that we know that our other tests are working.
    
    llvm-svn: 281700

---
+++ External/CUDA/assert.cu
@@ -0,0 +1,18 @@
+#undef NDEBUG
+
+#include <assert.h>
+#include <stdio.h>
+
+__global__ void kernel() {
+  // Our reference output contains the line number of this assert() call; be
+  // careful when modifying the parts of this file above this line.
+  assert(false);
+}
+
+int main() {
+  kernel<<<1,1>>>();
+  cudaError_t err = cudaDeviceSynchronize();
+  if (err != cudaErrorAssert)
+    return err;
+  return 0;
+}
