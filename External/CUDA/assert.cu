7ed33d93284fa6acbcfaff5edabe8f8e7c2c20ef
Artem Belevich <tra@google.com>
2016-12-13 20:52:08 +0000

    [test-suite] Test output should not depend on file path.
    llvm-svn: 289586

--- External/CUDA/assert.cu
+++ External/CUDA/assert.cu
@@ -1,6 +1,10 @@
 #include <assert.h>
 #include <stdio.h>
 
+// Use fixed value for __FILE__ so assert message does not depend on
+// the actual path to the file during compilation..
+#define __FILE__ "assert.cu"
+
 __global__ void kernel() {
   // Our reference output contains the line number of this assert() call; be
   // careful when modifying the parts of this file above this line.
