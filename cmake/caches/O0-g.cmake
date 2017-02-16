7d6c8edb2655b210d485632a9837cdb0900e4a08
Matthias Braun <matze@braunis.de>
2017-02-16 02:48:15 +0000

    cmake/caches: Add some caches with descriptive names
    Having the name describe the content makes it more obvious what is going
    when seeing logs.
    
    llvm-svn: 295275

---
+++ cmake/caches/O0-g.cmake
@@ -0,0 +1,5 @@
+# Lowest optimization level and maximum debug information; A typical developer
+# configuration.
+set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "")
+set(CMAKE_C_FLAGS_DEBUG "-O0 -g" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g" CACHE STRING "")
