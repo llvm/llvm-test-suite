5ca8cab3a9c351eb89f67a641a6eb0febf334e7c
Matthias Braun <matze@braunis.de>
2017-04-19 22:23:04 +0000

    ClamAV/zlib: Always include unistd.h to get declarations for things like open/close/lseek that gzip is using
    llvm-svn: 300774

--- MultiSource/Applications/ClamAV/zlib/zconf.h
+++ MultiSource/Applications/ClamAV/zlib/zconf.h
@@ -8,6 +8,9 @@
 #ifndef ZCONF_H
 #define ZCONF_H
 
+/* Modified for llvm test-suite: */
+#include <unistd.h>
+
 /*
  * If you *really* need a unique prefix for all types and library functions,
  * compile with -DZ_PREFIX. The "standard" zlib should be compiled without it.
