631efe19d35ae5f2338c89f7bc4c21e5ef5dcd75
Matthias Braun <matze@braunis.de>
2017-05-12 22:15:46 +0000

    cmake: Rework External handling
    When doing -DTEST_SUITE_SUBDIRS=External/skidmarks10 then
    External/CMakeLists.txt and the macros within would be skipped.
    Therefore move the llvm_externals_find() macro to a shared cmake module.
    
    llvm-svn: 302958

---
+++ cmake/modules/External.cmake
@@ -0,0 +1,26 @@
+set(TEST_SUITE_EXTERNALS_DIR "" CACHE PATH
+    "Directory containing test-suite external benchmark sources")
+
+# Find path containing an external benchmark and set PATHVAR to it.
+# Specifically this:
+# - Adds a CACHE variable for PATHVAR
+# - If PATHVAR is unset set it to the first existing directory in this list:
+#   - ${TEST_SUITE_EXTERNALS_DIR}/${NAME}
+#   - ${CMAKE_SOURCE_DIR}/test-suite-externals/${NAME}
+macro(llvm_externals_find PATHVAR NAME DESCRIPTION)
+  set(${PATHVAR} "" CACHE PATH "Directory containing ${DESCRIPTION} sourcecode")
+  if(NOT ${PATHVAR})
+    if(TEST_SUITE_EXTERNALS_DIR)
+      if (IS_DIRECTORY "${TEST_SUITE_EXTERNALS_DIR}/${NAME}")
+        set(${PATHVAR} "${TEST_SUITE_EXTERNALS_DIR}/${NAME}")
+      endif()
+    else()
+      if(IS_DIRECTORY "${CMAKE_SOURCE_DIR}/test-suite-externals/${NAME}")
+        set(${PATHVAR} "${CMAKE_SOURCE_DIR}/test-suite-externals/${NAME}")
+      endif()
+    endif()
+    if(${PATHVAR})
+      message(STATUS "Found ${DESCRIPTION}: ${${PATHVAR}}")
+    endif()
+  endif()
+endmacro()
