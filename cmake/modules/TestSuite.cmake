72383129f6284b6bcd2dc8185af76bc91c602f52
Matthias Braun <matze@braunis.de>
2017-08-02 15:58:45 +0000

    TestSuite.cmake: Improve comments; NFC
    llvm-svn: 309828

--- cmake/modules/TestSuite.cmake
+++ cmake/modules/TestSuite.cmake
@@ -5,9 +5,11 @@
 ##===----------------------------------------------------------------------===##
 include(TestFile)
 
-# Creates a new executable build target, applies CFLAGS, CPPFLAGS, CXXFLAGS
-# and LDFLAGS. Creates a .test file if possible and registers the target
-# with the TEST_SUITE_TARGETS list.
+# Creates a new executable build target. Use this instead of `add_executable`.
+# It applies CFLAGS, CPPFLAGS, CXXFLAGS and LDFLAGS. Creates a .test file if
+# necessary, registers the target with the TEST_SUITE_TARGETS list and makes
+# sure we build the required dependencies for compiletime measurements
+# and support the TEST_SUITE_PROFILE_USE mode.
 macro(llvm_test_executable target)
   add_executable(${target} ${ARGN})
   append_target_flags(COMPILE_FLAGS ${target} ${CFLAGS})
@@ -27,8 +29,9 @@ macro(llvm_test_executable target)
   test_suite_add_build_dependencies(${target})
 endmacro()
 
-# Creates a new library build target, applies CFLAGS, CPPFLAGS, CXXFLAGS
-# and LDFLAGS.
+# Creates a new library build target. Use this instead of `add_library`.
+# Behaves like `llvm_test_executable`, just produces a library instead of an
+# executable.
 macro(llvm_test_library target)
   add_library(${target} ${ARGN})
 
@@ -42,6 +45,16 @@ macro(llvm_test_library target)
   # TODO: How to support TEST_SUITE_PROFILE_USE properly?
 
   test_suite_add_build_dependencies(${target})
+endmacro()
+
+# Add dependencies required for compiletime measurements to a target. You
+# usually do not need to call this directly when using `llvm_test_executable`
+# or `llvm_test_library`.
+macro(test_suite_add_build_dependencies target)
+  if(NOT TEST_SUITE_USE_PERF)
+    add_dependencies(${target} timeit-target)
+  endif()
+  add_dependencies(${target} timeit-host fpcmp-host)
 endmacro()
 
 # Internal function that transforms a list of flags to a string and appends
@@ -64,13 +77,4 @@ macro(append_target_flags propertyname target)
     string(STRIP "${old_flags} ${quoted}" new_flags)
     set_target_properties(${target} PROPERTIES ${propertyname} "${new_flags}")
   endif()
-endmacro()
-
-# Internal function that adds the tools used for compiletime measurement as a
-# target.
-macro(test_suite_add_build_dependencies target)
-  if(NOT TEST_SUITE_USE_PERF)
-    add_dependencies(${target} timeit-target)
-  endif()
-  add_dependencies(${target} timeit-host fpcmp-host)
 endmacro()
