988fad796c6fb561e7e35d48de521434054716f6
Matthias Braun <matze@braunis.de>
2018-08-21 23:08:27 +0000

    cmake/litsupport: Remove TEST_SUITE_REMOTE_USER + TEST_SUITE_REMOTE_PORT
    Nobody is using these and you can always set them (and many
    other) options in your $HOME/.ssh/config file.
    
    llvm-svn: 340360

--- litsupport/modules/remote.py
+++ litsupport/modules/remote.py
@@ -14,12 +14,6 @@ def _mutateCommandline(context, commandline, suffix=""):
 
     config = context.config
     remote_commandline = config.remote_client
-    if config.remote_user:
-        remote_commandline += " -l %s" % config.remote_user
-    if config.remote_port:
-        remote_commandline += " -p %s" % config.remote_port
-    if config.remote_flags:
-        remote_commandline += config.remote_flags
     remote_commandline += " %s" % config.remote_host
     remote_commandline += " /bin/sh %s" % shfilename
     return remote_commandline
