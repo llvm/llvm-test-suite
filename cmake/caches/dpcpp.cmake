ad00dbf58758d013fd356d29a599edbd7152a4de
Vladimir Lazarev <vladimir.lazarev@intel.com>
2020-05-21 11:39:19 +0300

    [SYCL] dpc++ tests

---
+++ cmake/caches/dpcpp.cmake
@@ -0,0 +1,5 @@
+# Default dpcpp compiler configuration.
+
+# No extra command line arguments are needed to support SYCL
+set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
+set(SYCL_CXX_FLAGS "" CACHE STRING "")
