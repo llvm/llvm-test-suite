4bc08fcd46c937eb047de7ee4482476ffc1262b4
Yuanfang Chen <yuanfang.chen@sony.com>
2020-10-28 10:11:51 -0700

    tools/timeit.sh: remove thousand separator in  perf-stat task-clock line
    if there are any. If not, this is no-op.

--- tools/timeit.sh
+++ tools/timeit.sh
@@ -46,6 +46,6 @@ if [ "$APPEND_STATUS" = "1" ]; then
 fi
 
 echo exit $EXITCODE > $REPORT
-awk -F' ' '{if ($2 ~ /^task-clock/ || $3 ~ /^task-clock/) print "user",$1/1000; else if($2 == "seconds" && $4 == "elapsed") print "real",$1;}' $PERFSTAT >> $REPORT
+awk -F' ' '{if ($2 ~ /^task-clock/ || $3 ~ /^task-clock/) {gsub(/,/,"",$1);print "user",$1/1000;} else if($2 == "seconds" && $4 == "elapsed") print "real",$1;}' $PERFSTAT >> $REPORT
 
 exit $EXITCODE
