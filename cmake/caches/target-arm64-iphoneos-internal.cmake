3f9724e85f514c9d7014d8af00d22c7d8f1ffdf7
Mircea Trofin <mtrofin@google.com>
2021-07-19 07:52:41 -0700

    Disable benchmark tests on iOS
    This is a temporary build fix.
    
    Differential Revision: https://reviews.llvm.org/D106281

--- cmake/caches/target-arm64-iphoneos-internal.cmake
+++ cmake/caches/target-arm64-iphoneos-internal.cmake
@@ -1,3 +1,4 @@
 set(XCRUN_FLAGS --toolchain iOS --sdk iphoneos.internal)
 set(CMAKE_OSX_ARCHITECTURES arm64 CACHE STRING "")
+set(BENCHMARK_ENABLE_TESTING OFF)
 include(${CMAKE_CURRENT_LIST_DIR}/util/xcode_sdk.cmake)
--- cmake/caches/target-thumbv7s-iphoneos.cmake
+++ cmake/caches/target-thumbv7s-iphoneos.cmake
@@ -1,3 +1,4 @@
 set(XCRUN_FLAGS --toolchain iOS --sdk iphoneos)
 set(CMAKE_OSX_ARCHITECTURES thumbv7s CACHE STRING "")
+set(BENCHMARK_ENABLE_TESTING OFF)
 include(${CMAKE_CURRENT_LIST_DIR}/util/xcode_sdk.cmake)
