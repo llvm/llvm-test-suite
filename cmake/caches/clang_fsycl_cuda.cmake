53dcda47072214d89d84c5b5d4119c76984f5b62
Alexey Bader <alexey.bader@intel.com>
2021-09-24 13:17:50 +0300

    Update -fsycl-targets option values (#463)
    `-sycldevice` triple suffix is not needed anymore.
    This change should fix the warning emitted by the latest version of DPC++ compiler.

--- cmake/caches/clang_fsycl_cuda.cmake
+++ cmake/caches/clang_fsycl_cuda.cmake
@@ -1,4 +1,4 @@
 # Default open source clang configuration with SYCL support.
 
 set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
-set(CMAKE_CXX_FLAGS "-fsycl-targets=nvptx64-nvidia-cuda-sycldevice -Xsycl-target-backend --cuda-gpu-arch=sm_32" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "-fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --cuda-gpu-arch=sm_32" CACHE STRING "")
