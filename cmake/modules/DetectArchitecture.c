f3d50250fafaed394c5f8a9cc73863b5f4f8a9f5
Weining Lu <luweining@loongson.cn>
2022-11-17 19:06:50 +0800

    [test-suite] Add Architecture Detection for LoongArch
    The LLVM test suite has its own way of detecting the system
    architecture. This adds support in that file for detecting LoongArch.
    
    Reviewed By: MaskRay, xen0n
    
    Differential Revision: https://reviews.llvm.org/D138102

--- cmake/modules/DetectArchitecture.c
+++ cmake/modules/DetectArchitecture.c
@@ -4,6 +4,8 @@ const char *str = "ARCHITECTURE IS AArch64";
 const char *str = "ARCHITECTURE IS ARM";
 #elif defined(__alpha__)
 const char *str = "ARCHITECTURE IS Alpha";
+#elif defined(__loongarch__)
+const char *str = "ARCHITECTURE IS LoongArch";
 #elif defined(__mips__)
 const char *str = "ARCHITECTURE IS Mips";
 #elif defined(__powerpc__) || defined(__ppc__) || defined(__power__)
