426e6cb0d32dcacaa12239d5de2d15fa014c7e0f
Tom Eccles <tom.eccles@arm.com>
2023-08-01 17:59:02 +0000

    [Fortran][hlfir] update openmp denylists with new fixes
    Some tests have been fixed by
      - https://reviews.llvm.org/D127215
      - https://reviews.llvm.org/D156719
    
    Differential Revision: https://reviews.llvm.org/D156820

--- Fortran/gfortran/regression/gomp/DisabledFilesHLFIR.cmake
+++ Fortran/gfortran/regression/gomp/DisabledFilesHLFIR.cmake
@@ -5,12 +5,3 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
 #===------------------------------------------------------------------------===#
-
-file(GLOB SKIPPED_FILES CONFIGURE_DEPENDS
-  # error: operand#0 does not dominate this use
-  do-1.f90
-  nontemporal-1.f90
-  pr59488-1.f90
-  pr69183.f90
-  pr78298.f90
-)
--- Fortran/gfortran/regression/gomp/appendix-a/DisabledFilesHLFIR.cmake
+++ Fortran/gfortran/regression/gomp/appendix-a/DisabledFilesHLFIR.cmake
@@ -6,31 +6,10 @@
 #
 #===------------------------------------------------------------------------===#
 
-file(GLOB UNIMPLEMENTED_FILES CONFIGURE_DEPENDS
-  # (hlfir+omp): unimplemented: Common block in privatization clause
-  a.23.4.f90
-)
-
 file(GLOB SKIPPED_FILES CONFIGURE_DEPENDS
   #
   # These tests fail when they should pass.
   #
-
-  # (hlfir+omp): operand#0 does not dominate this use
-  a.1.1.f90
-  a.6.1.f90
-  a.8.1.f90
-  a.12.1.f90
-  a.21.2.f90
-  a.21.3.f90
-  a.26.2.f90
-  a.30.1.f90
-  a.31.1.f90
-  a.31.2.f90
-  a.34.1.f90
-  a.34.2.f90
-  a.35.2.f90
-  a.37.1.f90
 
   # (hlfir+omp): builder.create<DeclareOp>() assertion failure
   a.17.1.f90
