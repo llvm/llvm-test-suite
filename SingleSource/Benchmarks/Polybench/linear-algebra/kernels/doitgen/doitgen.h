ed58e2c4c5351b81046e67f749b10d72c50a61af
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2023-08-13 14:34:40 +0800

    [Polybench] Update doitgen
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

--- SingleSource/Benchmarks/Polybench/linear-algebra/kernels/doitgen/doitgen.h
+++ SingleSource/Benchmarks/Polybench/linear-algebra/kernels/doitgen/doitgen.h
@@ -1,60 +1,86 @@
 /**
- * doitgen.h: This file is part of the PolyBench/C 3.2 test suite.
+ * This version is stamped on May 10, 2016
  *
+ * Contact:
+ *   Louis-Noel Pouchet <pouchet.ohio-state.edu>
+ *   Tomofumi Yuki <tomofumi.yuki.fr>
  *
- * Contact: Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
  * Web address: http://polybench.sourceforge.net
  */
-#ifndef DOITGEN_H
-# define DOITGEN_H
+#ifndef _DOITGEN_H
+# define _DOITGEN_H
 
-/* Default to STANDARD_DATASET. */
-# if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
-#  define STANDARD_DATASET
+/* Default to LARGE_DATASET. */
+# if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(MEDIUM_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
+#  define LARGE_DATASET
 # endif
 
-/* Do not define anything if the user manually defines the size. */
 # if !defined(NQ) && !defined(NR) && !defined(NP)
-/* Define the possible dataset sizes. */
+/* Define sample dataset sizes. */
 #  ifdef MINI_DATASET
-#   define NQ 10
+#   define NQ 8
 #   define NR 10
-#   define NP 10
+#   define NP 12
 #  endif
 
 #  ifdef SMALL_DATASET
-#   define NQ 32
-#   define NR 32
-#   define NP 32
+#   define NQ 20
+#   define NR 25
+#   define NP 30
 #  endif
 
-#  ifdef STANDARD_DATASET /* Default if unspecified. */
-#   define NQ 128
-#   define NR 128
-#   define NP 128
+#  ifdef MEDIUM_DATASET
+#   define NQ 40
+#   define NR 50
+#   define NP 60
 #  endif
 
 #  ifdef LARGE_DATASET
-#   define NQ 256
-#   define NR 256
-#   define NP 256
+#   define NQ 140
+#   define NR 150
+#   define NP 160
 #  endif
 
 #  ifdef EXTRALARGE_DATASET
-#   define NQ 1000
-#   define NR 1000
-#   define NP 1000
+#   define NQ 220
+#   define NR 250
+#   define NP 270
 #  endif
-# endif /* !N */
+
+
+#endif /* !(NQ NR NP) */
 
 # define _PB_NQ POLYBENCH_LOOP_BOUND(NQ,nq)
 # define _PB_NR POLYBENCH_LOOP_BOUND(NR,nr)
 # define _PB_NP POLYBENCH_LOOP_BOUND(NP,np)
 
-# ifndef DATA_TYPE
-#  define DATA_TYPE double
-#  define DATA_PRINTF_MODIFIER "%0.2lf "
+
+/* Default data type */
+# if !defined(DATA_TYPE_IS_INT) && !defined(DATA_TYPE_IS_FLOAT) && !defined(DATA_TYPE_IS_DOUBLE)
+#  define DATA_TYPE_IS_DOUBLE
 # endif
 
+#ifdef DATA_TYPE_IS_INT
+#  define DATA_TYPE int
+#  define DATA_PRINTF_MODIFIER "%d "
+#endif
 
-#endif /* !DOITGEN */
+#ifdef DATA_TYPE_IS_FLOAT
+#  define DATA_TYPE float
+#  define DATA_PRINTF_MODIFIER "%0.2f "
+#  define SCALAR_VAL(x) x##f
+#  define SQRT_FUN(x) sqrtf(x)
+#  define EXP_FUN(x) expf(x)
+#  define POW_FUN(x,y) powf(x,y)
+# endif
+
+#ifdef DATA_TYPE_IS_DOUBLE
+#  define DATA_TYPE double
+#  define DATA_PRINTF_MODIFIER "%0.2lf "
+#  define SCALAR_VAL(x) x
+#  define SQRT_FUN(x) sqrt(x)
+#  define EXP_FUN(x) exp(x)
+#  define POW_FUN(x,y) pow(x,y)
+# endif
+
+#endif /* !_DOITGEN_H */
