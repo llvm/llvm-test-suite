d8a7fa5f614032e8738dde8ba848afa870eece14
Jerry Zhang Jian <jerry.zhangjian@sifive.com>
2023-08-20 21:36:06 +0800

    [Polybench] Update jacobi-2d
    Signed-off-by: Jerry Zhang Jian <jerry.zhangjian@sifive.com>

--- SingleSource/Benchmarks/Polybench/stencils/jacobi-2d-imper/jacobi-2d-imper.c
+++ SingleSource/Benchmarks/Polybench/stencils/jacobi-2d-imper/jacobi-2d-imper.c
@@ -1,10 +1,14 @@
 /**
- * jacobi-2d-imper.c: This file is part of the PolyBench/C 3.2 test suite.
+ * This version is stamped on May 10, 2016
  *
+ * Contact:
+ *   Louis-Noel Pouchet <pouchet.ohio-state.edu>
+ *   Tomofumi Yuki <tomofumi.yuki.fr>
  *
- * Contact: Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
  * Web address: http://polybench.sourceforge.net
  */
+/* jacobi-2d.c: this file is part of PolyBench/C */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -70,10 +74,10 @@ void kernel_jacobi_2d_imper(int tsteps,
     {
       for (i = 1; i < _PB_N - 1; i++)
 	for (j = 1; j < _PB_N - 1; j++)
-	  B[i][j] = 0.2 * (A[i][j] + A[i][j-1] + A[i][1+j] + A[1+i][j] + A[i-1][j]);
-      for (i = 1; i < _PB_N-1; i++)
-	for (j = 1; j < _PB_N-1; j++)
-	  A[i][j] = B[i][j];
+	  B[i][j] = SCALAR_VAL(0.2) * (A[i][j] + A[i][j-1] + A[i][1+j] + A[1+i][j] + A[i-1][j]);
+      for (i = 1; i < _PB_N - 1; i++)
+	for (j = 1; j < _PB_N - 1; j++)
+	  A[i][j] = SCALAR_VAL(0.2) * (B[i][j] + B[i][j-1] + B[i][1+j] + B[1+i][j] + B[i-1][j]);
     }
 #pragma endscop
 
@@ -96,10 +100,10 @@ kernel_jacobi_2d_imper_StrictFP(int tsteps,
     {
       for (i = 1; i < _PB_N - 1; i++)
 	for (j = 1; j < _PB_N - 1; j++)
-	  B[i][j] = 0.2 * (A[i][j] + A[i][j-1] + A[i][1+j] + A[1+i][j] + A[i-1][j]);
-      for (i = 1; i < _PB_N-1; i++)
-	for (j = 1; j < _PB_N-1; j++)
-	  A[i][j] = B[i][j];
+	  B[i][j] = SCALAR_VAL(0.2) * (A[i][j] + A[i][j-1] + A[i][1+j] + A[1+i][j] + A[i-1][j]);
+      for (i = 1; i < _PB_N - 1; i++)
+	for (j = 1; j < _PB_N - 1; j++)
+	  A[i][j] = SCALAR_VAL(0.2) * (B[i][j] + B[i][j-1] + B[i][1+j] + B[1+i][j] + B[i-1][j]);
     }
 }
 
