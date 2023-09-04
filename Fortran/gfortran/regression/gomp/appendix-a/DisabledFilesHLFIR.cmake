c7cc1a271974ac5c0cd16af6185a8174a9665a21
Tom Eccles <tom.eccles@arm.com>
2023-09-04 10:31:25 +0000

    [Fortran][hlfir] mark remaining openmp tests fixed
    Fixed by
    - https://reviews.llvm.org/D158594
    - https://reviews.llvm.org/D158070

--- Fortran/gfortran/regression/gomp/appendix-a/DisabledFilesHLFIR.cmake
+++ Fortran/gfortran/regression/gomp/appendix-a/DisabledFilesHLFIR.cmake
@@ -5,17 +5,3 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
 #===------------------------------------------------------------------------===#
-
-file(GLOB SKIPPED_FILES CONFIGURE_DEPENDS
-  #
-  # These tests fail when they should pass.
-  #
-
-  # (hlfir+omp): builder.create<DeclareOp>() assertion failure
-  a.17.1.f90
-  a.17.2.f90
-  a.17.3.f90
-
-  # (hlfir+omp): Fortran::lower::genThreadprivateOp assertion failure
-  a.22.1.f90
-)
