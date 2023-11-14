ad3ffc67bd12d92a9ec4f3b13b7f747e25f3f657
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2023-11-14 16:31:10 -0500

    simplify alloca test case by removing header file inclusion

--- SingleSource/UnitTests/2023-11-alloca.c
+++ SingleSource/UnitTests/2023-11-alloca.c
@@ -1,8 +1,8 @@
-#include <alloca.h>
+extern void *alloca(unsigned long sz);
 
 int main() {
   int sz = 8;
-  int *foo = alloca(sz);
+  int *foo = (int *)alloca(sz);
   foo[1] = 4;
   if (foo[1] == 4) {
     return 0;
