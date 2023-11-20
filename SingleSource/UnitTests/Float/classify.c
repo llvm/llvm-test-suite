35f5a79028c1bc5699bcd9e844a413c637e28bab
Serge Pavlov <sepavloff@gmail.com>
2023-11-21 01:48:59 +0700

    Tests for additional builtin classification functions
    This change adds tests for functions 'iszero', 'issubnornal' and 'issignaling'.
    It also adds tests for Intel fp80 type and some other small enhancements.
    
    Differential Revision: https://reviews.llvm.org/D112933

--- SingleSource/UnitTests/Float/classify.c
+++ SingleSource/UnitTests/Float/classify.c
@@ -12,15 +12,20 @@
 
 #include "classify-f32.h"
 #include "classify-f64.h"
+#include "classify-f80.h"
 #include "classify-ldouble.h"
 
-
-int main()
-{
+int main() {
   test_float();
+  test_isfpclass_float();
   test_double();
+  test_isfpclass_double();
   prepare_ldouble_tables();
   test_ldouble();
+  test_isfpclass_ldouble();
+#ifdef USE_X86_FP80_TYPE
+  test_fp80();
+  test_isfpclass_fp80();
+#endif
   return 0;
 }
-
