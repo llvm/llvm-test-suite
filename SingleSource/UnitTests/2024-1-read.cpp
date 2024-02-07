35c890871b91f84f1f53cfda9ffd6261fdd8aef1
thealmarty <thealmartyblog@gmail.com>
2024-02-07 14:05:22 -0800

    Add cpp tests.

---
+++ SingleSource/UnitTests/2024-1-read.cpp
@@ -0,0 +1,9 @@
+int main() {
+  int x = __builtin_delendum_read_advice();
+  if (x == 0) {
+    while (1) {
+    }
+  } else {
+    return 0;
+  }
+}
---
+++ SingleSource/UnitTests/2024-1-write.cpp
@@ -0,0 +1,5 @@
+int main() {
+  int v = 65;
+  __builtin_delendum_write(v);
+  return 0;
+}
