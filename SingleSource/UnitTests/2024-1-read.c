856e3152b7caffa77d195091f0015ac3b7773a6f
thealmarty <thealmartyblog@gmail.com>
2024-02-07 11:41:21 -0800

    Update read.

--- SingleSource/UnitTests/2024-1-read.c
+++ SingleSource/UnitTests/2024-1-read.c
@@ -1,5 +1,9 @@
-int main()
-{
-    __builtin_delendum_read_advice();
+int main() {
+  int x = __builtin_delendum_read_advice();
+  if (x == 0) {
+    while (1) {
+    }
+  } else {
     return 0;
+  }
 }
