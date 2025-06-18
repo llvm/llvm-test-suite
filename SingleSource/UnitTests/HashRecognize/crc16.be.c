2720be48fc72da4affe15a20c708a335c2d999b3
Ramkumar Ramachandra <ramkumar.ramachandra@codasip.com>
2025-06-18 21:23:12 +0100

    [HashRecognize] Add tests to verify CRC optz (#258)
    In preparation to optimize cyclic-redundancy-check loops using
    table-lookup using the newly introduced HashRecognize analysis, add
    tests with CRC loops (which will get optimized), and check the result
    against a Sarwate table-lookup (which will not get optimized). The
    companion change is llvm/llvm-project#143208.

---
+++ SingleSource/UnitTests/HashRecognize/crc16.be.c
@@ -0,0 +1,58 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#define GENPOLY 4129
+
+static uint16_t CRCTable[256];
+
+static void crc_init(void) {
+  uint16_t crc = 0x8000;
+  for (unsigned int i = 1; i < 256; i <<= 1) {
+    crc = (crc << 1) ^ (crc & 0x8000 ? GENPOLY : 0);
+    for (unsigned int j = 0; j < i; j++)
+      CRCTable[i + j] = crc ^ CRCTable[j];
+  }
+}
+
+// This table-lookup should be equivalent to the code emitted when optimizing
+// CRC with HashRecognize. This function itself will be untouched by
+// HashRecognize.
+static uint16_t crc_table(uint16_t crc_initval, uint16_t data) {
+  uint16_t crc = crc_initval;
+
+  if (CRCTable[255] == 0)
+    crc_init();
+
+  for (size_t i = 0; i < 2; ++i) {
+    uint16_t pos = (crc >> 8) ^ (data << (i << 3) >> 8);
+    crc = (crc << 8) ^ CRCTable[pos & 0xFF];
+  }
+
+  return crc;
+}
+
+static uint16_t crc_loop(uint16_t crc_initval, uint16_t data) {
+  uint16_t crc = crc_initval;
+
+  // This loop will be optimized by HashRecognize.
+  for (size_t i = 0; i < 16; ++i) {
+    uint16_t xor_crc_data = crc ^ data;
+    uint16_t crc_shl = crc << 1;
+    crc = (xor_crc_data & 0x8000) ? (crc_shl ^ GENPOLY) : crc_shl;
+    data <<= 1;
+  }
+  return crc;
+}
+
+int main() {
+  // These are random hand-picked values.
+  static const uint16_t crc_initval[8] = {0, 129, 11, 255, 16, 4129, 16384, 1};
+  static const uint16_t data[8] = {0, 1, 11, 16, 129, 255, 4129, 16384};
+  for (size_t i = 0; i < 8; ++i) {
+    uint16_t actual = crc_loop(crc_initval[i], data[i]);
+    uint16_t expected = crc_table(crc_initval[i], data[i]);
+    if (actual != expected)
+      return 1;
+  }
+  return 0;
+}
