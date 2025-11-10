4c7d2a5be749e1674a441e4795261eb93d1e80da
Marc Auberer <marc.auberer@sap.com>
2025-11-10 10:32:38 +0000

    Apply feedback

--- Bitcode/Regression/sap_fct_splitting/mocks.c
+++ Bitcode/Regression/sap_fct_splitting/mocks.c
@@ -1,14 +1,10 @@
-typedef unsigned long long uint64_t;
-typedef long long int64_t;
-typedef unsigned int uint32_t;
+#include <stdint.h>
+
 typedef char* opaque_ptr;
 
 // Globals
 opaque_ptr _debugInfo = 0;
 opaque_ptr _sourceCodeObj = 0;
-
-// Helper functions
-void disrupt_optimizer() {}
 
 // Mock functions
 uint64_t llangStackTracePushVoid(opaque_ptr context, uint32_t lineNumber, uint64_t returnCode, opaque_ptr literal, opaque_ptr debugInfo, opaque_ptr sourceCodeObj) {
