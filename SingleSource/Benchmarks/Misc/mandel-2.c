f97e854d554105700f215297b64e1dab60d0ffdd
Dean Sturtevant <dsturtevant@google.com>
2025-12-11 11:26:50 -0500

    Replace "complex" with "complex double".
    This fixes diagnostics with the message
    "plain '_Complex' requires a type specifier; assuming '_Complex double'".
    NOTE: There is no warning option assoicated with these diagnostics.

--- SingleSource/Benchmarks/Misc/mandel-2.c
+++ SingleSource/Benchmarks/Misc/mandel-2.c
@@ -6,10 +6,10 @@ int max_i = 65536;
 
 double sqr(double x) { return x*x; }
 
-double cnorm2(complex z) { return sqr(creal(z)) + sqr(cimag(z)); }
+double cnorm2(complex double z) { return sqr(creal(z)) + sqr(cimag(z)); }
 
-int loop(complex c) {
-    complex z=c;
+int loop(complex double c) {
+    complex double z=c;
     int i=1;
     while (cnorm2(z) <= 4.0 && i++ < max_i)
         z = z*z + c;
