886d54411c71894880b19f4ea66b35d676a361a4
Florian Hahn <flo@fhahn.com>
2026-01-07 10:03:03 +0000

    !fixup address comments, thanks

--- SingleSource/UnitTests/Vectorizer/early-exit.cpp
+++ SingleSource/UnitTests/Vectorizer/early-exit.cpp
@@ -9,6 +9,12 @@
 template <typename Ty>
 using TestFnTy = std::function<unsigned(std::function<void(Ty *)>)>;
 
+// Helper to call \p f with \p args and acts as optimization barrier for \p f.
+template <typename F, typename... Args>
+__attribute__((optnone)) static auto callThroughOptnone(F &&f, Args &&...args) {
+  return f(std::forward<Args>(args)...);
+}
+
 template <typename Ty>
 static void checkVectorFunction(TestFnTy<Ty> ScalarFn, TestFnTy<Ty> VectorFn,
                                 TestFnTy<Ty> InterleavedFn, const char *Name) {
@@ -19,17 +25,17 @@ static void checkVectorFunction(TestFnTy<Ty> ScalarFn, TestFnTy<Ty> VectorFn,
 
   // Check finding the target element at all indices between 0 and 512.
   for (unsigned IdxToFind = 0; IdxToFind < 512; ++IdxToFind) {
-    // Labmda to initialize all array elements to one, except the one to look
+    // Lambda to initialize all array elements to one, except the one to look
     // for to zero.
-    auto Init1 = [IdxToFind](int *Arr) {
+    auto Init1 = [IdxToFind](Ty *Arr) {
       std::fill_n(Arr, N, 1);
       if (IdxToFind < N)
         Arr[IdxToFind] = 0;
     };
 
-    // Labmda to initialize all array elements to one, except the one to look
+    // Lambda to initialize all array elements to one, except the one to look
     // for and the IdxToFind + 3 to zero.
-    auto Init2 = [IdxToFind](int *Arr) {
+    auto Init2 = [IdxToFind](Ty *Arr) {
       std::fill_n(Arr, N, 1);
       if (IdxToFind < N)
         Arr[IdxToFind] = 0;
@@ -41,15 +47,15 @@ static void checkVectorFunction(TestFnTy<Ty> ScalarFn, TestFnTy<Ty> VectorFn,
     auto Reference2 = ScalarFn(Init2);
     // Run vector functions and check against the scalar result.
     for (const auto &[Fn, Name] : Tests) {
-      auto ToCheck1 = Fn(Init1);
+      auto ToCheck1 = callThroughOptnone(Fn, Init1);
       if (Reference1 != ToCheck1) {
         std::cerr << "Miscompare for " << Name << ": " << Reference1
                   << " != " << ToCheck1 << "\n";
         exit(1);
       }
-      auto ToCheck2 = Fn(Init2);
+      auto ToCheck2 = callThroughOptnone(Fn, Init2);
       if (Reference2 != ToCheck2) {
-        std::cerr << "Miscompare for " << Name << ": " << Reference1
+        std::cerr << "Miscompare for " << Name << ": " << Reference2
                   << " != " << ToCheck2 << "\n";
         exit(1);
       }
@@ -63,15 +69,18 @@ static void checkVectorFunction(TestFnTy<Ty> ScalarFn, TestFnTy<Ty> VectorFn,
 /// * one where VF is picked automatically and interleaving is forced.
 #define DEFINE_SCALAR_AND_VECTOR_EARLY_EXIT(Init, Src, Loop)                   \
   auto ScalarFn = [](std::function<void(int *)> II) -> unsigned {              \
-    Init II(Src);                                                              \
+    Init;                                                                      \
+    II(Src);                                                                   \
     _Pragma("clang loop vectorize(disable) interleave_count(1)") Loop          \
   };                                                                           \
   auto VectorFn = [](std::function<void(int *)> II) -> unsigned {              \
-    Init II(Src);                                                              \
+    Init;                                                                      \
+    II(Src);                                                                   \
     _Pragma("clang loop vectorize(enable)") Loop                               \
   };                                                                           \
   auto InterleavedFn = [](std::function<void(int *)> II) -> unsigned {         \
-    Init II(Src);                                                              \
+    Init;                                                                      \
+    II(Src);                                                                   \
     _Pragma("clang loop vectorize(enable) interleave_count(4)") Loop           \
   };
 
